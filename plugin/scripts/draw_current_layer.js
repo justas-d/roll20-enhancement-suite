91340b6e9e0cbcd28baa2b64005d1a14571468d7
Justas Dabrila <justasdabrila@gmail.com>
2018-07-27 23:53:00 +0000

    only inject draw_current_layer if player is gm

--- plugin/scripts/draw_current_layer.js
+++ plugin/scripts/draw_current_layer.js
@@ -1,45 +1,46 @@
-let div = document.createElement("div");
+if(window.is_gm) {
+    let div = document.createElement("div");
 
-div.style.height ="auto";
-div.style.marginBottom ="15px";
-div.style.marginRight ="15px";
-div.style.width ="auto";
-div.style.maxWidth ="100%";
-div.style.overflowX ="hidden";
-div.style.position ="absolute";
-div.style.bottom ="0";
-div.style.right ="0";
-div.style.backgroundClip = "border-box";
+    div.style.height ="auto";
+    div.style.marginBottom ="15px";
+    div.style.marginRight ="15px";
+    div.style.width ="auto";
+    div.style.maxWidth ="100%";
+    div.style.overflowX ="hidden";
+    div.style.position ="absolute";
+    div.style.bottom ="0";
+    div.style.right ="0";
+    div.style.backgroundClip = "border-box";
 
-let text = document.createElement("p");
-text.style.fontFamily=  "Helvetica";
-text.style.fontSize= "26px";
+    let text = document.createElement("p");
+    text.style.fontFamily=  "Helvetica";
+    text.style.fontSize= "26px";
 
 
-function render(layer) {
-    let data = window.r20es.getCustomLayerData(layer);
+    function render(layer) {
+        let data = window.r20es.getCustomLayerData(layer);
 
-    text.innerHTML = data.bigTxt;
-    div.style.backgroundColor = data.bg;
+        text.innerHTML = data.bigTxt;
+        div.style.backgroundColor = data.bg;
+    }
+
+    div.appendChild(text);
+
+    render(window.currentEditingLayer);
+
+    document.getElementById("playerzone").appendChild(div);
+
+    function callback(ctx) { 
+        let l = "";
+
+        if(ctx.target.className === "choosegmlayer") { l = "gmlayer"; }
+        else if(ctx.target.className === "choosemap") { l = "map"; }
+        else if(ctx.target.className === "chooseobjects") { l = "objects"; }
+
+        render(l); 
+    }
+
+    $("#editinglayer li.chooseobjects").on("click", callback);
+    $("#editinglayer li.choosemap").on("click", callback);
+    $("#editinglayer li.choosegmlayer").on("click", callback);
 }
-
-div.appendChild(text);
-
-render(window.currentEditingLayer);
-
-document.getElementById("playerzone").appendChild(div);
-
-function callback(ctx) { 
-    let l = "";
-
-    if(ctx.target.className === "choosegmlayer") { l = "gmlayer"; }
-    else if(ctx.target.className === "choosemap") { l = "map"; }
-    else if(ctx.target.className === "chooseobjects") { l = "objects"; }
-
-    render(l); 
-}
-
-$("#editinglayer li.chooseobjects").on("click", callback);
-$("#editinglayer li.choosemap").on("click", callback);
-$("#editinglayer li.choosegmlayer").on("click", callback);
-
