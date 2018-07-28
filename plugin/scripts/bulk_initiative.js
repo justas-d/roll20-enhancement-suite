c8e9f3c21978e5b27be2b41216ad3866d07bee02
Justas Dabrila <justasdabrila@gmail.com>
2018-07-28 00:50:33 +0000

    initial bulk_initiative

---
+++ plugin/scripts/bulk_initiative.js
@@ -0,0 +1,11 @@
+window.d20.engine.canvas.on("selection:created", e => {
+    console.log(e);
+});
+
+/*
+window.d20.engine.canvas.on("object:selected", e => {
+    console.log(e);
+});
+*/
+
+console.log("event set!");
