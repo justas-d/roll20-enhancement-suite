a1266e93bf8598d26cb051b68edc17c0e2ede30a
Justas Dabrila <justasdabrila@gmail.com>
2018-08-09 21:20:01 +0000

    dialogApi: added internalRender, rerender, get/setData

--- src/tools/dialogApi.js
+++ src/tools/dialogApi.js
@@ -1,26 +1,43 @@
 import { createElement } from "./createElement";
 import { removeAllChildren, findByIdAndRemove } from "./miscUtil";
 
+
 class DialogBase {
     constructor() {
         window.r20esDialogId = "r20esDialogId" in window ? window.r20esDialogId : 0;
         this.numId = window.r20esDialogId++;
         this.id = `r20es-dialog-${this.numId}`;
-        this.root = createElement("dialog", {id: this.id});
+        this.root = createElement("dialog", { id: this.id });
 
         this.show = this.show.bind(this);
-        this.close= this.close.bind(this);
+        this.close = this.close.bind(this);
 
-        //document.body.appendChild(this.root);
         document.body.insertBefore(this.root, document.body.firstElementChild);
         dialogPolyfill.registerDialog(this.root);
     }
 
-    render() {}
+    render() { }
+
+    internalRender() {
+        this.root.appendChild(this.render());
+        dialogPolyfill.reposition(this.root);
+    }
+
+    rerender() {
+        removeAllChildren(this.root);
+        this.internalRender();
+    }
 
     show() {
-        this.root.appendChild(this.render());
+        this.internalRender();
         this.root.showModal();
+    }
+
+    setData = data => this.returnData = data;
+    getData = _ => { 
+        const temp = this.returnData;
+        this.returnData = null;
+        return temp;
     }
 
     close() {
@@ -31,7 +48,7 @@ class DialogBase {
     dispose() {
         findByIdAndRemove(this.id);
     }
-    
+
 }
 
 export { DialogBase }
