b7bd350f115e8acf79ebd63a795fbd2855c4f796
Justas Dabrila <justasdabrila@gmail.com>
2018-08-09 12:46:04 +0000

    bootstrappers: execute in bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,21 +1,27 @@
 import { Config } from "./tools/config";
-import { hooks } from "./hooks";
+import { hooks} from "./hooks";
+import { safeCall } from "./tools/miscUtil";
+import { DialogFormsBootstrapper } from './modules/dialogForms.js';
+import { getBrowser } from "./tools/webExtHelpers";
+
+console.log("=================");
+console.log("window.r20es bootstrap");
+console.log("=================");
+
+window.bootstrapTable = {};
+
+new DialogFormsBootstrapper().bootstrap();
 
 function injectScript(name) {
     console.log(`Injecting ${name}`);
 
     var s = document.createElement("script");
     s.async = false;
-    s.src = (chrome || browser).extension.getURL(`js/${name}`);
+    s.src = getBrowser().extension.getURL(`js/${name}`);
 
     s.onload = () => { s.remove(); };
     document.head.appendChild(s);
 }
-
-
-console.log("=================");
-console.log("window.r20es bootstrap");
-console.log("=================");
 
 // inject global environment
 injectScript("globals.js");
@@ -41,7 +47,18 @@ for(let hookId in hooks) {
 
     if(!("filename" in hook)) continue;
     
-    injectScript(hook.filename);
+    if(hook.filename in window.bootstrapTable) {
+        const boot = window.bootstrapTable[hook.filename];
+
+        safeCall(_ => boot.beforeInject());
+        injectScript(hook.filename);
+        safeCall(_ => boot.afterInject());
+        
+    } else {
+        injectScript(hook.filename);
+    }
+    
 }
 
+window.bootstrapTable = undefined;
 console.log("r20es bootstrap done");
