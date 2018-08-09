0bc1a8d19f4ee553ac74bb35f2d7fcdd3ade3641
Justas Dabrila <justasdabrila@gmail.com>
2018-08-09 21:18:45 +0000

    r20bootstapper: added injectCSS/script

--- src/modules/dialogForms.js
+++ src/modules/dialogForms.js
@@ -1,6 +1,5 @@
 import { R20Bootstrapper } from "../tools/r20bootstrapper";
 import { getBrowser } from "../tools/webExtHelpers";
-import { createElement } from "../tools/createElement";
 import { removeAllChildren } from "../tools/miscUtil";
 
 class DialogFormsBootstrapper extends R20Bootstrapper.Base {
@@ -22,20 +21,8 @@ class DialogFormsBootstrapper extends R20Bootstrapper.Base {
         const css = getBrowser().runtime.getURL("thirdparty/dialog-polyfill.css");
         const js = getBrowser().runtime.getURL("thirdparty/dialog-polyfill.js");
 
-        createElement("link", {
-            rel: "stylesheet",
-            id: this.cssId,
-            type: "text/css",
-            href: css,
-        }, null, document.head);
-
-
-
-        createElement("script", {
-            async: false,
-            src: js,
-            id: this.jsId
-        }, null, document.body);
+        this.injectCSS(css, document.head, this.cssId);
+        this.injectScript(js, document.body, this.jsId, false);
 
         console.log("DialogFormsBootstrapper: done!");
     }
