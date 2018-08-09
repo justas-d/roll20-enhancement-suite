9aac56f4a65bc8acd76c876ce3780041724af82d
Justas Dabrila <justasdabrila@gmail.com>
2018-08-09 14:04:03 +0000

    macro-generator: initial dialog

--- src/modules/macro-generator.js
+++ src/modules/macro-generator.js
@@ -1,44 +1,67 @@
 import { R20Module } from "../tools/r20Module";
 import { R20 } from "../tools/r20api";
 import { OGL5eByRoll20MacroGenerator } from "../macro/OGL5eByRoll20.js";
+import { DialogBase } from "../tools/dialogApi";
+import { createElementJsx } from "../tools/createElement";
 
 const generateButtonId = "r20esgenerate"
+
+class GenerateMacrosDialog extends DialogBase {
+    constructor(generators) {
+        super();
+        this.generators = generators;
+    }
+
+    render() {
+        return (
+            <div>
+               <button class="btn" onClick={this.close}>Close</button>
+            </div>
+        )
+    }
+}
 
 class MacroGeneratorModule extends R20Module.SimpleBase {
     constructor(id) {
         super(id);
-        this.getAllActions = this.getAllActions.bind(this);
-        this.onButtonClick = this.onButtonClick.bind(this);
-
         this.generators = {};
-    
+
         const addGen = gen => this.generators[gen.id] = gen;
         addGen(OGL5eByRoll20MacroGenerator);
+
+        this.getAllActions = this.getAllActions.bind(this);
+        this.onButtonClick = this.onButtonClick.bind(this);
     }
 
     getAllActions(char) {
-        
+
         const factories = OGL5eByRoll20MacroGenerator.macroFactories;
         let data = [];
 
-        for(let factoryId in factories) {
+        for (let factoryId in factories) {
             const factory = factories[factoryId];
             data = data.concat(factory(char));
         }
 
-        return data;        
+        return data;
     }
 
     onButtonClick(e) {
-        console.log(e);
+        this.dialog.show();
     }
 
     setup() {
+        this.dialog = new GenerateMacrosDialog(this.generators);
         window.r20es.macroGeneratorButtonClick = this.onButtonClick;
+        window.r20es.temp = this.onButtonClick;
+    }
+
+    dispose() {
+        this.dialog.dispose()
     }
 }
 
-if(R20Module.canInstall()) new MacroGeneratorModule(__filename).install();
+if (R20Module.canInstall()) new MacroGeneratorModule(__filename).install();
 
 const hook = R20Module.makeHook(__filename, {
     id: "macroGeneratorBase",
@@ -59,10 +82,10 @@ const hook = R20Module.makeHook(__filename, {
             patch: `<button class='btn ${generateButtonId}'>Generate</button><button class='btn addabil'>+ Add</button>`,
         }
     ],
-    
+
     config: {
         lastGenerator: OGL5eByRoll20MacroGenerator.id
     }
 });
 
-export {hook as MacroGeneratorHook}
+export { hook as MacroGeneratorHook }
