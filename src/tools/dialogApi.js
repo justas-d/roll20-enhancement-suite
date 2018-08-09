02d678a00e9e28bc58cff7e236231a8dd03351a1
Justas Dabrila <justasdabrila@gmail.com>
2018-08-09 14:03:04 +0000

    dialogApi: properly rerender

--- src/tools/dialogApi.js
+++ src/tools/dialogApi.js
@@ -6,12 +6,12 @@ class DialogBase {
         window.r20esDialogId = "r20esDialogId" in window ? window.r20esDialogId : 0;
         this.numId = window.r20esDialogId++;
         this.id = `r20es-dialog-${this.numId}`;
-        this.needsRender = true;
         this.root = createElement("dialog", {id: this.id});
 
         this.show = this.show.bind(this);
         this.close= this.close.bind(this);
 
+        //document.body.appendChild(this.root);
         document.body.insertBefore(this.root, document.body.firstElementChild);
         dialogPolyfill.registerDialog(this.root);
     }
@@ -19,17 +19,12 @@ class DialogBase {
     render() {}
 
     show() {
-        if(this.needsRender) {
-            removeAllChildren(this.root);
-            const elem = this.render();
-            console.log(elem);
-            this.root.appendChild(elem);
-        }
-
+        this.root.appendChild(this.render());
         this.root.showModal();
     }
 
     close() {
+        removeAllChildren(this.root);
         this.root.close();
     }
 
