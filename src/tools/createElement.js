a16d5168777075a3235960dfa40a9510921c8427
Justas Dabrila <justasdabrila@gmail.com>
2018-08-11 21:00:28 +0000

    createElement: use appendChild when rerendering if nextTo is undefined

--- src/tools/createElement.js
+++ src/tools/createElement.js
@@ -1,16 +1,20 @@
-
 class ElementBase {
-    render() {}
+    render() { }
 
     rerender() {
+
         const nextTo = this.elementRoot.nextSibling;
         const parent = this.elementRoot.parentNode;
 
         this.elementRoot.remove();
-        
+
         const elem = this.render();
+        if (nextTo) {
+            parent.insertBefore(elem, nextTo);
+        } else {
+            parent.appendChild(elem);
+        }
         this.setRoot(elem);
-        parent.insertBefore(elem, nextTo);
     }
 
     setRoot = root => this.elementRoot = root;
@@ -19,24 +23,24 @@ class ElementBase {
 function createElementJsx(type, attributes, ...children) {
 
     let elem = null;
-    const isFunc = typeof(type) === "function";
+    const isFunc = typeof (type) === "function";
 
-    if(isFunc && type.name && type.prototype instanceof ElementBase) {
+    if (isFunc && type.name && type.prototype instanceof ElementBase) {
         let base = new type(attributes);
         elem = base.render();
         base.setRoot(elem);
-    } else if(isFunc) {
+    } else if (isFunc) {
         elem = type(attributes);
     } else {
         elem = document.createElement(type);
     }
 
-    if(!(elem instanceof HTMLElement)) {
+    if (!(elem instanceof HTMLElement)) {
         console.error($`JSX Render must return a valid element: elem is not an instance of HTMLElement`);
 
         console.table({
             [`"type" argument`]: type,
-            "Type": typeof(elem),
+            "Type": typeof (elem),
             "Value": elem
         });
 
@@ -49,16 +53,16 @@ function createElementJsx(type, attributes, ...children) {
 
         function recursiveAddChildren(recursiveChildren) {
             for (let child of recursiveChildren) {
-                if(child instanceof HTMLElement)  {
+                if (child instanceof HTMLElement) {
                     frag.appendChild(child);
-                } else if(Array.isArray(child)) {
+                } else if (Array.isArray(child)) {
                     recursiveAddChildren(child);
-                } else if(typeof(child) === "string") {
+                } else if (typeof (child) === "string") {
                     frag.appendChild(document.createTextNode(child));
 
-                // values that we assume are control flow related
-                } else if(child === null || child === undefined || typeof(child) === "boolean") {
-                    console.warn(`JSX got an unrenderable child value, assuming it's control flow related: type: ${typeof(child)} value: ${child}.`);
+                    // values that we assume are control flow related
+                } else if (child === null || child === undefined || typeof (child) === "boolean") {
+                    console.warn(`JSX got an unrenderable child value, assuming it's control flow related: type: ${typeof (child)} value: ${child}.`);
                 } else {
                     console.error(`Unknown child type while proecssing JSX: ${child}`);
                 }
@@ -66,7 +70,7 @@ function createElementJsx(type, attributes, ...children) {
         }
 
         recursiveAddChildren(children);
-        
+
         elem.appendChild(frag);
     }
 
@@ -83,14 +87,14 @@ function createElementJsx(type, attributes, ...children) {
                 for (let className of val) {
                     elem.classList.add(className);
                 }
-            } else if(typeof(val) === "string" && val.length > 0) {
+            } else if (typeof (val) === "string" && val.length > 0) {
                 elem.className = (elem.className && elem.classList.length > 0)
                     ? `${elem.className} ${val}`
                     : val;
             }
         } else if (attribId === "style") {
             for (let elemId in val) {
-                if(!val) continue;
+                if (!val) continue;
                 elem.style[elemId] = val[elemId];
             }
         } else {
@@ -136,7 +140,7 @@ function createElement(type, _attributes, children, parent) {
         } else if (attribId === "checked") {
             elem.checked = val;
         } else if (attribId === "style") {
-            if(!val) continue;
+            if (!val) continue;
             for (let elemId in val) {
                 elem.style[elemId] = val[elemId];
             }
