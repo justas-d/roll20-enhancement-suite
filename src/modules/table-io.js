7783ce52a4194aeec610f08bb799117efe6c7b84
Justas Dabrila <justasdabrila@gmail.com>
2018-08-11 21:30:32 +0000

    table-io: hotloading improvements

--- src/modules/table-io.js
+++ src/modules/table-io.js
@@ -2,7 +2,7 @@ import { R20Module } from "../tools/r20Module";
 import { R20 } from "../tools/r20api";
 import { TableIO } from "../tools/table-io";
 import { saveAs } from 'save-as'
-import { createSidebarSeparator, createElement, createElementJsx } from "../tools/createElement";
+import { createElementJsx, SidebarSeparator } from "../tools/createElement";
 import { readFile, safeParseJson } from "../tools/fileUtil";
 import { TableExportLang } from "../tools/table-export-lang";
 import { findByIdAndRemove } from "../tools/miscUtil";
@@ -14,50 +14,67 @@ class TableIOModule extends R20Module.OnAppLoadBase {
         super(id);
 
         this.journalDivId = "r20es-tableio-journal-widget";
-        this.buttonClass = "r20es-export-table-button"
+        this.tableWidgetClass = "r20es-export-table-button"
+
+        this.normalImportButtonId = "r20es-norma;-import-button";
+        this.tableExportImportButtonId = "r20es-table-export-import-button";
+
         this.observerCallback = this.observerCallback.bind(this);
         this.onExportButtonClicked = this.onExportButtonClicked.bind(this);
+        this.onImportClicked = this.onImportClicked.bind(this);
+        this.onFileChanged = this.onFileChanged.bind(this);
     }
 
     getTableId(target) {
-        const query = $(target.parentNode.parentNode).find(`div[${tableIdAttribute}]`);
-        if(query.length <= 0) return null;
+        // target must be the header with all the fancy classes that we match
+        const query = $(target.parentNode).find(`div[${tableIdAttribute}]`);
+        if (query.length <= 0) return null;
 
         const elem = query[0];
-        if(!elem.hasAttribute(tableIdAttribute)) return null;
-        return elem.getAttribute(tableIdAttribute)
+        if (!elem.hasAttribute(tableIdAttribute)) return null;
+
+        return elem.getAttribute(tableIdAttribute);
     }
 
     onExportButtonClicked(e) {
-        let tableId = this.getTableId(e.target);
-        if (!tableId) { alert("Failed to get table id."); return;}
+        let tableId = this.getTableId(e.target.parentNode); 
+        if (!tableId) { alert("Failed to get table id."); return; }
 
         let table = R20.getRollableTable(tableId);
         if (!table) { alert(`Failed to get table. Table id: ${tableId}`); return; }
-        
+
         let data = TableIO.exportJson(table);
 
         let jsonBlob = new Blob([data], { type: 'data:application/javascript;charset=utf-8' });
         saveAs(jsonBlob, table.get("name") + ".json");
     }
 
+    tryInsertTableWidget(target) {
+        if (!target.className) return false;
+        if (target.className !== "ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix") return false;
+        if (target.getElementsByClassName(this.tableWidgetClass).length > 0) return false;
+
+        const table = this.getTableId(target);
+        console.log(target);
+        console.log(table);
+
+        if (!table) return false;
+
+        const button = <button
+            style={{ marginTop: "8px" }}
+            onClick={this.onExportButtonClicked}
+            className={[this.tableWidgetClass, "btn"]}>
+            Export
+            </button>;
+
+        target.appendChild(button);
+        return true;
+    }
+
     observerCallback(muts) {
         for (var e of muts) {
-            if (e.target.className && e.target.className === "ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix") {
-
-                if (e.target.getElementsByClassName(this.buttonClass).length > 0) return;
-                if(!this.getTableId(e.target)) return;
-
-                const button = <button
-                    style={{ marginTop: "8px" }}
-                    onClick={this.onExportButtonClicked}
-                    className={[this.buttonClass, "btn"]}>
-                    Export
-                </button>;
-
-                e.target.appendChild(button);
-
-                return;
+            if(this.tryInsertTableWidget(e.target)) {
+                break;
             }
         }
     }
@@ -80,73 +97,83 @@ class TableIOModule extends R20Module.OnAppLoadBase {
         }
     }
 
+    onImportClicked(e) {
+
+        let cb = e.target.id === this.tableExportImportButtonId
+            ? this.importTablesTableExport
+            : this.importTableJson;
+
+        const input = $(e.target.parentNode).find("input")[0];
+
+        readFile(input.files[0], (e2) => {
+            cb(e2.target.result);
+        });
+
+        input.value = "";
+        this.setButtonDisabled(e.target.parentNode, true);
+    }
+
+    setButtonDisabled(root, state) {
+        let query = $(root).find(".btn");
+        query.each(idx => {
+            query[idx].disabled = state;
+        });
+    }
+
+    onFileChanged(e) {
+        this.setButtonDisabled(e.target.parentNode, e.target.files.length <= 0);
+    }
+
     setup() {
         if (!R20.isGM()) return;
 
+        // @COPYPASTE from character-io
+        const existingHeaders = document.querySelectorAll(".ui-dialog-titlebar, .ui-widget-header, .ui-corner-all,  .ui-helper-clearfix");
+
+        for(const header of existingHeaders) {
+            this.tryInsertTableWidget(header);
+        }
+    
+        let root = document.getElementById("deckstables").getElementsByClassName("content")[0];
+
+        const buttonStyle = { marginBottom: "10px", marginRight: "10px", float: "left" };
+        const elem = <div id={this.journalDivId}>
+            <SidebarSeparator />
+            <h3 style={{ marginBottom: "5px", marginLeft: "5px" }}>
+                Import Rollable Table
+            </h3>
+
+            <input onChange={this.onFileChanged} type="file" />
+
+            <button id={this.normalImportButtonId} onClick={this.onImportClicked} disabled className="btn" style={buttonStyle}>
+                Import
+            </button>
+
+            <button id={this.tableExportImportButtonId} onClick={this.onImportClicked} disabled className="btn" style={buttonStyle}>
+                Import (TableExport)
+            </button>
+        </div >
+
+        root.appendChild(elem);
+
         this.observer = new MutationObserver(this.observerCallback);
         this.observer.observe(document.body, { childList: true, subtree: true });
-
-        let rootOfDiv = document.getElementById("deckstables").getElementsByClassName("content")[0];
-
-        function setButtonDisabled(root, state) {
-            let query = $(root).find(".btn");
-            query.each(idx => {
-                query[idx].disabled = state;
-            });
-        }
-
-        function mkCallback(cb) {
-            return e => {
-                const input = $(e.target.parentNode).find("input")[0];
-
-                readFile(input.files[0], (e2) => {
-                    cb(e2.target.result);
-                });
-
-                input.value = "";
-                setButtonDisabled(e.target.parentNode, true);
-            }
-        };
-
-        createElement("div", { id: this.journalDivId }, [
-            () => { return createSidebarSeparator(); },
-
-            createElement("h3", {
-                innerHTML: "Import Rollable Table",
-                style: {
-                    marginBottom: "5px",
-                    marginLeft: "5px"
-                }
-            }),
-            createElement("input", {
-                type: "file",
-                onChange: (e) => { setButtonDisabled(e.target.parentNode, e.target.files.length <= 0); }
-            }),
-            createElement("button", {
-                innerHTML: "Import",
-                disabled: true,
-                className: "btn",
-                style: {
-                    float: "left"
-                },
-                onClick: mkCallback((e) => { this.importTableJson(e) })
-            }),
-            createElement("button", {
-                innerHTML: "Import (TableExport)",
-                disabled: true,
-                className: "btn",
-                style: {
-                    float: "left",
-                },
-                onClick: mkCallback((e) => this.importTablesTableExport(e))
-            })
-
-        ], rootOfDiv);
     }
 
     dispose() {
         if (this.observer) {
             this.observer.disconnect();
+        }
+
+        // @COPYPASTE from character-io
+        const widgets = document.getElementsByClassName(this.tableWidgetClass);
+
+        
+        // removing a widget modifies the widgets html element collection 
+        // therefore we have to treat this as a stack
+        let num = widgets.length;
+        while(num --> 0) {
+            widgets[0].remove();
         }
 
         findByIdAndRemove(this.journalDivId);
