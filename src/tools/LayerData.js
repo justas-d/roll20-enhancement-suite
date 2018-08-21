dc5060d8ca6018838ea1c80e12015abd365684ec
Justas Dabrila <justasdabrila@gmail.com>
2018-08-21 17:13:13 +0000

    LayerData: add class, makeBgStyle

--- src/tools/LayerData.js
+++ src/tools/LayerData.js
@@ -1,32 +1,24 @@
 import { R20 } from "./R20";
 
-function getLayerData(layer) {
-    if (layer === R20.layer.map) return {
-        selector: "#editinglayer li.choosemap",
-        bigTxt: "Map Background",
-        txt: "MP",
-        bg: "rgba(255,255,0,0.5)"
+class LayerData {
+    constructor(selector, bigTxt, txt, bgValues) {
+        this.selector = selector;
+        this.bigTxt = bigTxt;
+        this.txt = txt;
+        this.bgValues = bgValues;
     }
 
-    else if (layer === R20.layer.playerTokens) return {
-        selector: "#editinglayer li.chooseobjects",
-        bigTxt: "Tokens (Player Visible)",
-        txt: "TK",
-        bg: "rgba(255,0,0,0.5)"
+    makeBgStyle(alpha) {
+        return `rgba(${this.bgValues[0]}, ${this.bgValues[1]}, ${this.bgValues[2]}, ${alpha})`;
     }
-    else if (layer === R20.layer.gmTokens) return {
-        selector: "#editinglayer li.choosegmlayer",
-        bigTxt: "Game Master Tokens",
-        txt: "GM",
-        bg: "rgba(0,255,0,0.5)"
-    }
-    
-    return {
-        selector: "",
-        bigTxt: layer,
-        txt: layer,
-        bg: "rgba(255,255,255,0.5)"
-    }
+}
+
+function getLayerData(layer) {
+    if (layer === R20.layer.map)                    return new LayerData("#editinglayer li.choosemap", "Map Background", "MP", [255, 255, 0]);
+    else if (layer === R20.layer.playerTokens)      return new LayerData("#editinglayer li.chooseobjects","Tokens (Player Visible)","TK",[255, 0, 0]);
+    else if (layer === R20.layer.gmTokens)          return new LayerData("#editinglayer li.choosegmlayer","Game Master Tokens","GM",[0, 255, 0]);
+
+    return new LayerData("",layer,layer,[255, 255, 255]);
 }
 
 export { getLayerData }
