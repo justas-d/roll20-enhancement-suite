41914af549757682669ad0f32df4e9203a9b3393
Justas Dabrila <justasdabrila@gmail.com>
2018-09-04 13:42:58 +0000

    ChangelogWidget: implement

--- src/modules/ChangelogWidget.tsx
+++ src/modules/ChangelogWidget.tsx
@@ -1,18 +1,25 @@
 import { DOM } from "../tools/DOM";
+import { strIsNullOrEmpty } from "../tools/MiscUtils";
 
 declare namespace build {
     export const R20ES_CHANGELOG: string;
     export const R20ES_VERSION: string;
 }
 
-const ChangelogWidget = function(props: any) {
+const ChangelogWidget = function (props: any) {
     const paragraphs = build.R20ES_CHANGELOG.split('\n');
-        
+
+    const lis = [];
+    for(const p of paragraphs) {
+        if(strIsNullOrEmpty(p)) continue
+        lis.push(<li>{p}</li>);
+    }
+    
     return (
-        <div>
+        <ul>
             <h2>{build.R20ES_VERSION}</h2>
-            {paragraphs.map(text => <p>{text}</p>)}
-        </div>
+            {lis}
+        </ul>
     );
 }
 
