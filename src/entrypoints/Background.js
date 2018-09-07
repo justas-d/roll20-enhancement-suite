b5953453f83275297960de5e9f94d72777cad142
Justas Dabrila <justasdabrila@gmail.com>
2018-09-07 08:07:10 +0000

    Background: fix broken image urls on firefox

--- src/entrypoints/Background.js
+++ src/entrypoints/Background.js
@@ -43,21 +43,24 @@ const injectHooks = (intoWhat, hookQueue, replaceFunc) => {
     return intoWhat;
 }
 
+window.redirectTargets = [
+    "https://app.roll20.net/v2/js/jquery",
+    "https://app.roll20.net/js/featuredetect.js",
+    "https://app.roll20.net/editor/startjs",
+    "https://app.roll20.net/js/jquery",
+    "https://app.roll20.net/js/d20/loading.js",
+    "https://app.roll20.net/assets/firebase",
+    "https://app.roll20.net/assets/base.js",
+    "https://app.roll20.net/assets/app.js",
+    "https://app.roll20.net/js/tutorial_tips.js",
+];
+
+const isRedirectTarget = (url) => typeof(window.redirectTargets.find(f => url.startsWith(f))) !== "undefined";
+
 if (isChrome()) {
 
     window.redirectCount = 0;
     window.hasBeenRedirected = {};
-    window.redirectTargets = [
-        "https://app.roll20.net/v2/js/jquery",
-        "https://app.roll20.net/js/featuredetect.js",
-        "https://app.roll20.net/editor/startjs",
-        "https://app.roll20.net/js/jquery",
-        "https://app.roll20.net/js/d20/loading.js",
-        "https://app.roll20.net/assets/firebase",
-        "https://app.roll20.net/assets/base.js",
-        "https://app.roll20.net/assets/app.js",
-        "https://app.roll20.net/js/tutorial_tips.js",
-    ];
 
     const setupEnvironment = appUrl => {
 
@@ -186,7 +189,7 @@ if (isChrome()) {
             console.log("RESET REDIRECT TABLE");
             window.hasBeenRedirected = {};
             window.redirectCount = 0;
-        } else if (window.redirectTargets.find(f => dt.url.startsWith(f))) {
+        } else if (isRedirectTarget(dt.url)) {
             if (window.hasBeenRedirected[dt.url]) {
                 console.log(`SKIP ${dt.url}`);
                 return;
@@ -247,13 +250,20 @@ if (isChrome()) {
     // thanks, Firefox.
     window.requestListener = function (dt) {
         
+        
+        const isRedir = isRedirectTarget(dt.url);
+        console.log(`${isRedir}: ${dt.url}`);
+        if(!isRedir) return;
+
         const hookQueue = getHooks(configs, dt.url);
         const filter = getBrowser().webRequest.filterResponseData(dt.requestId);
         const decoder = new TextDecoder("utf-8");
 
         // Note(Justas): the console.log here forces scripts to run in order
         // and not randomly, avoiding race conditions
-        let stringBuffer = dt.url.includes("js") ? `console.log("running ${dt.url}");window.enhancementSuiteEnabled = true;` : "";
+        // Along with that, this place is the earliest we can set 
+        // window.enhancementSuiteEnabled = true
+        let stringBuffer = `console.log("running ${dt.url}");window.enhancementSuiteEnabled = true;`;
 
         filter.ondata = e => {
             stringBuffer += decoder.decode(e.data, { stream: true });
