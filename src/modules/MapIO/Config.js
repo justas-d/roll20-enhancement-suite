828387afdb54a5fc12466e969a56505ac128c1b7
Justas Dabrila <justasdabrila@gmail.com>
2018-09-10 12:20:35 +0000

    MapIO: fix id name conflict with MacroIO

--- src/modules/MapIO/Config.js
+++ src/modules/MapIO/Config.js
@@ -1,7 +1,7 @@
 import MakeConfig from '../MakeConfig'; import Category from '../Category';
 
 export default MakeConfig(__dirname, {
-    id: "macroIO",
+    id: "mapIO",
     name: "Map Importer/Exporter",
     description: "Allows exporting and importing of maps",
     category: Category.exportImport,
