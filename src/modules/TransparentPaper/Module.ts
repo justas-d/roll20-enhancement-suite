472dee84d50acd23f4ad2070e04e022c6cccd084
Justas Dabrila <justasdabrila@gmail.com>
2018-09-11 23:36:43 +0000

    TransparentPaperModule: refactor to ts

---
+++ src/modules/TransparentPaper/Module.ts
@@ -0,0 +1,37 @@
+import { R20Module } from "../../tools/R20Module"
+import { findByIdAndRemove, createCSSElement } from "../../tools/MiscUtils";
+
+class TransparentPaperModule extends R20Module.OnAppLoadBase {
+    private static readonly styleId = "r20es-transparent-paper-style";
+
+    constructor() {
+        super(__dirname);
+    }
+
+    private static removeStyle() {
+        findByIdAndRemove(TransparentPaperModule.styleId);
+    }
+
+    private injectStyle() {
+        const cfg = this.getHook().config;
+
+        const el = createCSSElement(`.ui-dialog { opacity: ${cfg.opacity}; }`, TransparentPaperModule.styleId);
+        document.body.appendChild(el);
+    }
+
+    protected onSettingChange(name: string, oldVal: any, newVal: any) {
+        TransparentPaperModule.removeStyle();
+        this.injectStyle();
+    }
+
+    public setup() {
+        this.injectStyle();
+    }
+
+    public dispose() {
+        TransparentPaperModule.removeStyle();
+        super.dispose();
+    }
+}
+
+if (R20Module.canInstall()) new TransparentPaperModule().install();
