ddddea4563f945a5589b0a25b9b3adc7f849cd72
Justas Dabrila <justasdabrila@gmail.com>
2018-09-15 09:07:27 +0000

    MapIO: add media, description

--- src/modules/MapIO/Config.js
+++ src/modules/MapIO/Config.js
@@ -3,7 +3,11 @@ import MakeConfig from '../MakeConfig'; import Category from '../Category';
 export default MakeConfig(__dirname, {
     id: "mapIO",
     name: "Map Importer/Exporter",
-    description: "Allows exporting and importing of maps",
+    description: "Lets you export and import maps across campaigns.",
     category: Category.exportImport,
+
+    media: {
+        "map_io.png": "The Map I/O widget found in \"My Settings\""
+    }
 
 });
