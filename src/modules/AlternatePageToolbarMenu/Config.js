0bc03c585105bd4081d9f74c5a696dbadcd8a92b
Justas Dabrila <justasdabrila@gmail.com>
2018-09-19 18:40:18 +0000

    AlternatePageToolbarMenu: update desc

--- src/modules/AlternatePageToolbarMenu/Config.js
+++ src/modules/AlternatePageToolbarMenu/Config.js
@@ -5,7 +5,7 @@ import ConfigViews from '../../tools/ConfigViews';
 export default MakeConfig(__dirname, {
     id: "alternativePageToolbarMenu",
     name: "Alternative Page Toolbar Menu",
-    description: "Replaces the default page toolbar menu with a more concise one.",
+    description: "Replaces the default page toolbar menu with a more concise one. Click on the thumbnail of the page to switch to it.",
     category: Category.canvas,
 
     media: {
