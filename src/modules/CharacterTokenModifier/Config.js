8b1e2ed1a0cef056430f138ab76dfb0ad29d2fca
Justas Dabrila <justasdabrila@gmail.com>
2018-09-30 19:45:44 +0000

    TokenEditor: fix typo (thanks, DoomRice)

--- src/modules/CharacterTokenModifier/Config.js
+++ src/modules/CharacterTokenModifier/Config.js
@@ -3,7 +3,7 @@ import MakeConfig from '../MakeConfig'; import Category from '../Category';
 export default MakeConfig(__dirname, {
     id: "characterTokenModifier",
     name: "Character Token Editor",
-    description: "Adds a tab to character sheeds that provides a quick way to create and modify character tokens.",
+    description: "Adds a tab to character sheets that provides a quick way to create and modify character tokens.",
     category: Category.token,
 
     media: {
