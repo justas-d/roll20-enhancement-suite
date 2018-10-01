acd2a63db2ae6a940d0612255a51eba1bc746698
Justas Dabrila <justasdabrila@gmail.com>
2018-10-01 12:31:59 +0000

    About: improve brand icon positions, add discord, bmc

--- src/modules/Settings/AboutDialog.tsx
+++ src/modules/Settings/AboutDialog.tsx
@@ -41,7 +41,10 @@ export default class AboutDialog extends DialogBase<null> {
         return (
             <Dialog>
                 <DialogHeader style={{textAlign: "center"}}>
-                    <h1>Roll20 Enhancement Suite</h1>
+                    <a href={"javascript:void(0) // workaround for underpopup dialog from roll20 regarding leaving the site"}
+                       onClick={() => this.openUrl(Config.website)}>
+                        <h1 style="color: blue" >Roll20 Enhancement Suite</h1>
+                    </a>
                     <h2>Version {build.R20ES_VERSION}</h2>
                     <h3>Built for {build.R20ES_BROWSER}</h3>
                 </DialogHeader>
@@ -61,44 +64,36 @@ export default class AboutDialog extends DialogBase<null> {
                     </div>
 
                     <div style={{marginTop: "16px", marginBottom: "16px", textAlign: "center"}}>
-                        <div style={{marginBottom: "8px"}}>
+
+                        <span style={{marginRight: "8px"}}>
+                            <a href={"javascript:void(0) // workaround for underpopup dialog from roll20 regarding leaving the site"}
+                               onClick={() => this.openUrl(Config.discordInvite)}>
+                                <img height="32" width="32" src="https://discordapp.com/assets/41484d92c876f76b20c7f746221e8151.svg"/>
+                            </a>
+                        </span>
+
+                        <span style={{marginRight: "8px"}}>
                             <a href={"javascript:void(0) // workaround for underpopup dialog from roll20 regarding leaving the site"}
                                onClick={() => this.openUrl("https://github.com/SSStormy/roll20-enhancement-suite/")}>
                                 <img height="32" width="32"
                                      src="https://unpkg.com/simple-icons@latest/icons/github.svg"/>
                             </a>
-                        </div>
+                        </span>
 
-                        <div>
+                        <span>
                             <a href={"javascript:void(0) // workaround for underpopup dialog from roll20 regarding leaving the site"}
-                               onClick={() => this.openUrl(Config.discordInvite)}>
-                                Discord Server
+                               onClick={() => this.openUrl(Config.buyMeACoffee)}>
+                                <img height="32" width="32" src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee"/>
                             </a>
-                        </div>
-                    </div>
+                        </span>
 
-                    <div style={{marginTop: "16px", display: "flex", justifyContent: "center", alignItems: "center"}}>
-
-                        <style>
-                            {`
-                            .bmc-button
-                            img{width: 27px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{line - height: 36px !important;height:37px !important;text-decoration: none !important;display:inline-flex !important;color:#FFFFFF !important;background-color:#FF813F !important;border-radius: 3px !important;border: 1px solid transparent !important;padding: 0px 9px !important;font-size: 17px !important;letter-spacing:-0.08px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Lato', sans-serif !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover,
-                            .bmc-button:active,
-                            .bmc-button:focus {-webkit - box - shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#FFFFFF !important;}
-                        `}
-                        </style>
-
-                        <a className="bmc-button" target="_blank" href="https://www.buymeacoffee.com/stormy">
-                            <img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee"/>
-                            <span style="margin-top: 4px; margin-left:5px">Buy me a coffee</span>
-                        </a>
                     </div>
 
                 </DialogBody>
 
                 <section style={{margin: "20px"}}>
                     <input
-                        className="btn"
+                        className="btn btn-primary"
                         style={{width: "100%", height: "auto", boxSizing: "border-box"}}
                         type="button"
                         onClick={this.close}
