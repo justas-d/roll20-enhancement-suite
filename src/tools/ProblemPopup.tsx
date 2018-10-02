0422e6f8853b162925ca15b2e842d552b396dc72
Justas Dabrila <justasdabrila@gmail.com>
2018-10-02 21:11:09 +0000

    ProblemPopup: add a suggestion to open a new tab

--- src/tools/ProblemPopup.tsx
+++ src/tools/ProblemPopup.tsx
@@ -7,7 +7,8 @@ export default function showProblemPopup(debugCode) {
         <div className="r20es-welcome">
             <h2>R20ES - Uh oh!</h2>
             <p>Looks like something's not working. There might be a bug/problem somewhere.</p>
-            <p>Try refereshing the page.</p>
+            <p>Try refreshing the page.</p>
+            <p>Try closing this tab and opening the editor in a new one.</p>
             <p>If this persists, please visit the <a href={Config.discordInvite}>Discord server</a> and let us know.</p>
 
             <p className="r20es-code">
