dfda5c2ece0120dfead014168cc4471a1b34119b
Justas Dabrila <justasdabrila@gmail.com>
2018-10-03 23:38:00 +0000

    contributors: add Blurn Glanstone

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -2,3 +2,4 @@
 * **Jay "Vanguard" Fothergill** - logo ideas & consulting.
 * Mike - A coffee
 * Aaron - A coffee
+* Blurn Glanstone - 5 coffees
--- src/modules/Settings/AboutDialog.tsx
+++ src/modules/Settings/AboutDialog.tsx
@@ -70,7 +70,7 @@ export default class AboutDialog extends DialogBase<null> {
                             </div>
 
                             <div>
-                                Mike, Aaron
+                                Mike, Aaron, Blurn Glanstone
                             </div>
                         </div>
 
