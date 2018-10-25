9b7307ad8a32afb678d8183f46619a5f1aed777d
Justas Dabrila <justasdabrila@gmail.com>
2018-10-25 05:11:34 +0000

    AnimatedBackground: update description

--- src/modules/AnimatedBackgroundLayer/Config.js
+++ src/modules/AnimatedBackgroundLayer/Config.js
@@ -5,9 +5,9 @@ import ConfigViews from '../../utils/ConfigViews';
 export default MakeConfig(__dirname, {
     id: "animatedBackground",
     name: "Animated Background",
-    description: "Provides an animate",
+    description: "Displays an animated background if the GM has one set up for the page. Setup can be found in the top-left corner, look for a orange film button.",
     category: Category.canvas,
-
+    
     includes: "assets/app.js",
     find: `var o=e/d20.engine.canvasZoom;`,
     patch: `>>R20ES_MOD_FIND>>if(window.r20es && window.r20es.onZoomChange) window,r20es.onZoomChange(e);`,
