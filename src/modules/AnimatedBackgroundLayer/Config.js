b8b4234bb9dab64062483ebcbd7a9bb359701fc5
Justas Dabrila <justasdabrila@gmail.com>
2018-11-07 05:38:07 +0000

    added animated_bg media

--- src/modules/AnimatedBackgroundLayer/Config.js
+++ src/modules/AnimatedBackgroundLayer/Config.js
@@ -7,7 +7,11 @@ export default MakeConfig(__dirname, {
     name: "Animated Background",
     description: "Displays an animated background if the GM has one set up for the page. Setup can be found in the top-left corner, look for a orange film button.",
     category: Category.canvas,
-    
+
+    media: {
+        "animated_bg.webm": "Setup & usage"
+    },
+
     includes: "assets/app.js",
     find: `var o=e/d20.engine.canvasZoom;`,
     patch: `>>R20ES_MOD_FIND>>if(window.r20es && window.r20es.onZoomChange) window,r20es.onZoomChange(e);`,
