0ee71288f60677029fe67d11b78a74f85067f2fa
Justas Dabrila <justasdabrila@gmail.com>
2019-01-17 14:57:53 +0000

    DisablePlayerDrawings: final config

--- src/modules/DisablePlayerDrawings/Config.js
+++ src/modules/DisablePlayerDrawings/Config.js
@@ -3,7 +3,11 @@ import MakeConfig from '../MakeConfig'; import Category from '../Category';
 export default MakeConfig(__dirname, {
     id: "disablePlayerDrawing",
     name: "Disable Player Drawing",
-    description: "Disables the drawing tools for certain players. Only the GM needs to have the extension installed for this to work.",
+    description: "Disables the drawing/text tools for certain players. Only the GM needs to have the extension installed for this to work.",
     category: Category.canvas,
     gmOnly: true,
+
+    media: {
+        "disable_drawing.png": "Disable/Enable drawing permission button"
+    }
 });
