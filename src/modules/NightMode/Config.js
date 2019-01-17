c70129cb02b62f4441aa89619e60e61e61e4c6a3
Justas Dabrila <justasdabrila@gmail.com>
2019-01-17 15:09:31 +0000

    NightMode: remove all the styling, keep only the bg force

--- src/modules/NightMode/Config.js
+++ src/modules/NightMode/Config.js
@@ -3,12 +3,8 @@ import ConfigViews from "../../utils/ConfigViews";
 
 export default MakeConfig(__dirname, {
     id: "nightMode",
-    name: "Night Mode (Force dark background)",
-    description: "The Night Mode is being phased out in favor of Red Reign's Dark Theme",
-
-    urls: {
-        "You can find it here": "https://github.com/RedReign/Roll20-Dark-Theme"
-    },
+    name: "Force dark background",
+    description: "Force a dark background",
 
     category: Category.canvas,
 
@@ -17,25 +13,10 @@ export default MakeConfig(__dirname, {
             display: "Force the canvas background to be black?",
             type: ConfigViews.Checkbox,
         },
-        ogl5ESheet: {
-            display: "(Deprecated) OGL D&D 5E Night Mode ",
-            type: ConfigViews.Checkbox,
-        },
-        styleChat: {
-            display: "(Deprecated) Style chat?",
-            type: ConfigViews.Checkbox,
-        },
-        baseStyle: {
-            display: "(Deprecated) Base style (disable this if you only want a dark canvas)",
-            type: ConfigViews.Checkbox,
-        }
     },
 
     config: {
-        baseStyle: false,
         overrideBackground: true,
         enabled: false,
-        ogl5ESheet: false,
-        styleChat: false,
     },
 });
--- src/modules/NightMode/Module.tsx
+++ src/modules/NightMode/Module.tsx
@@ -1,19 +1,8 @@
 import {R20Module} from '../../utils/R20Module'
-import {findByIdAndRemove} from '../../utils/MiscUtils';
 import {R20} from "../../utils/R20";
 import {DOM} from "../../utils/DOM";
 
-const css = require("./nightMode.scss");
-const ogl5ecss = require("./5eOGLNightMode.scss");
-const noSheetCss = require("./noCharacterSheetColor.scss");
-const chatCss = require("./chatMessages.scss");
-
 class DarkModeModule extends R20Module.OnAppLoadBase {
-    private static readonly styleId = "r20es-dark-mode-style-id";
-    private static readonly ogl5estyleId = "r20es-dark-mode-ogl-5e-style-id";
-    private static readonly noSheetStyleId = "r20es-dark-mode-no-sheets-id";
-    private static readonly chatStyleId = "r20es-dark-mode-chat-style-id";
-
     constructor() {
         super(__dirname);
     }
@@ -37,64 +26,25 @@ class DarkModeModule extends R20Module.OnAppLoadBase {
     private resetPageBackground() {
         const page = R20.getCurrentPage();
         R20.setBackgroundStyle(page.attributes.background_color);
-    }
 
-    private addStyleWidget(style: string, id: string) {
-        const widget = <style id={id}>{style}</style>;
-        document.head.appendChild(widget);
-    }
-
-    private createWidget() {
-
-        const cfg = this.getHook().config;
-
-        if (cfg.baseStyle) {
-            this.addStyleWidget(css, DarkModeModule.styleId);
-        }
-
-        if (cfg.styleChat) {
-            this.addStyleWidget(chatCss, DarkModeModule.chatStyleId);
-        }
-
-        if (cfg.ogl5ESheet) {
-            this.addStyleWidget(ogl5ecss, DarkModeModule.ogl5estyleId);
-        } else {
-            this.addStyleWidget(noSheetCss, DarkModeModule.noSheetStyleId);
-        }
-
-        this.updatePageBackground();
-    }
-
-    private removeWidget() {
-        findByIdAndRemove(DarkModeModule.styleId);
-        findByIdAndRemove(DarkModeModule.chatStyleId);
-        findByIdAndRemove(DarkModeModule.ogl5estyleId);
-        findByIdAndRemove(DarkModeModule.noSheetStyleId);
     }
 
     public onSettingChange(name, oldVal, newVal) {
         this.updatePageBackground();
-        this.removeWidget();
-        this.createWidget();
     }
 
     public setup() {
-        this.createWidget();
+
         $(document).on("d20:pagechanged", this.onPageChanged);
 
-        const cfg = this.getHook().config;
-        if (cfg.baseStyle || cfg.styleChat || cfg.ogl5ESheet) {
-            R20.saySystem(`
-<h2 style="color: #c94a2a">Night Mode Deprecation</h2>
-<span>Night Mode is being phased out in favor of <a <style="color: orange;" href="https://github.com/RedReign/Roll20-Dark-Theme">Red Reign's Dark Theme<a/></span>`
-            );
-        }
+        setTimeout(() => {
+            this.updatePageBackground();
+        }, 500);
     }
 
     public dispose() {
         super.dispose();
         $(document).off("d20:pagechanged", this.onPageChanged);
-        this.removeWidget();
 
         this.resetPageBackground();
         R20.renderAll();
