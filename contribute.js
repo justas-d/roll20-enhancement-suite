ff26b2f4e5f0947553b6529cecda81ebaa7fd8cd
Justas Dabrila <justasdabrila@gmail.com>
2020-06-19 23:15:24 +0300

    bmc

---
+++ contribute.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="0; url=https://ssstormy.github.io/coffee.html" />
+
+    <title>Contribute | VTTES</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  </head>
+
+  <body>
+    <div id="root">
+      <a href="https://ssstormy.github.io/coffee.html">If you are not being redirected, click here.</a>
+    </div>
+  </body>
+
+</html>
