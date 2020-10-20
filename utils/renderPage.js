6074ff388d2e007d34696855e3ea302d35525777
Justas Dabrila <justasdabrila@gmail.com>
2020-10-20 23:00:50 +0300

    1.15.32 chrome

--- utils/renderPage.js
+++ utils/renderPage.js
@@ -57,5 +57,5 @@ addStaticFile("logo.svg", "./assets/logo/logo.svg");
 addStaticFile("main.css", "./page/main.css");
 addStaticFile("more.css", "./page/more.css");
 addStaticFile("takedown.png", "./page/takedown.png");
-addStaticFile("r20es_1.15.31_chrome.zip", "./page/r20es_1.15.31_chrome.zip");
+addStaticFile("r20es_1.15.32_chrome.zip", "./page/r20es_1.15.32_chrome.zip");
 
