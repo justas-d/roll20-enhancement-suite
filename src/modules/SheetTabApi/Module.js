e5eb4c1cd10fa9fbdd0becd0cb35406dfcb808de
Justas Dabrila <justasdabrila@gmail.com>
2021-03-18 22:39:03 +0200

    SheetTabApi: use iframe injection; detect if we're injecting duplicate tabs

--- src/modules/SheetTabApi/Module.js
+++ src/modules/SheetTabApi/Module.js
@@ -2,195 +2,276 @@ import { R20Module } from '../../utils/R20Module'
 import { DOM } from '../../utils/DOM'
 import { SheetTab } from '../../utils/SheetTab';
 import { R20 } from "../../utils/R20";
+import { promiseWait } from "../../utils/promiseWait";
 
 const charIdAttribute = "data-characterid";
 
 class SheetTabApiModule extends R20Module.OnAppLoadBase {
-    constructor() {
-        super(__dirname);
+  constructor() {
+    super(__dirname);
 
-        this.observerCallback = this.observerCallback.bind(this);
-        this.tryInjectingSingleWidget = this.tryInjectingSingleWidget.bind(this);
-        this.tryInjectingWidget = this.tryInjectingWidget.bind(this);
-        this.navOnClick = this.navOnClick.bind(this);
-        this.onClickNormalNavs = this.onClickNormalNavs.bind(this);
-        this.rescan = this.rescan.bind(this);
+    this.observerCallback = this.observerCallback.bind(this);
+    this.try_injecting_single_widget = this.try_injecting_single_widget.bind(this);
+    this.try_injecting_widget = this.try_injecting_widget.bind(this);
+    this.navOnClick = this.navOnClick.bind(this);
+    this.onClickNormalNavs = this.onClickNormalNavs.bind(this);
+    this.rescan = this.rescan.bind(this);
 
-        this.tabStyle = "r20es-character-sheet-tab";
-        this.attribNavHasListener = "data-r20es-character-sheet-nav-event";
-        this.attribCustomNav = "data-r20es-nav";
+    this.tabStyle = "r20es-character-sheet-tab";
+    this.attribNavHasListener = "data-r20es-character-sheet-nav-event";
+    this.attribCustomNav = "data-r20es-nav";
 
-        // bookkeeping for disposal
-        this.infectedNavs = [];
+    // bookkeeping for disposal
+    this.infectedNavs = [];
+  }
+
+  getWidgetTabRoots(navAElement) {
+    return $(navAElement.parentNode.parentNode.parentNode).find("." + this.tabStyle);
+  }
+
+  unselectSyntheticNavs(e) {
+    const navTabsRoot = e.target.parentNode.parentNode;
+    $(navTabsRoot).find("a[data-r20es-nav]").each((i, el) => {
+      el.parentNode.classList.remove("active");
+    });
+  }
+
+  onClickNormalNavs(e) {
+    this.unselectSyntheticNavs(e);
+    this.getWidgetTabRoots(e.target).each((i, obj) => {
+      obj.style.display = "none";
+    });
+  }
+
+  navOnClick(e) {
+    const targetTabClass = e.target.getAttribute("data-tab");
+
+    const internalTabs = SheetTab._getInternalData();
+    const tab = internalTabs.tabsById[targetTabClass];
+
+    const char_id = e.target.getAttribute(charIdAttribute);
+    const tabInstance = tab.getInstanceData(char_id);
+
+    //console.log(tabInstance);
+
+    if(tab && tab.onShow) {
+      tab.onShow(tabInstance);
     }
 
-    getWidgetTabRoots(navAElement) {
-        return $(navAElement.parentNode.parentNode.parentNode).find("." + this.tabStyle);
+    this.unselectSyntheticNavs(e);
+    e.target.parentNode.classList.add("active");
+
+    this.getWidgetTabRoots(e.target).each((i, obj) => {
+      obj.style.display = obj.classList.contains(targetTabClass)
+        ? "block"
+        : "none";
+    });
+  }
+
+  try_injecting_widget(target) {
+    const data = SheetTab._getInternalData();
+
+    for (const tab of data.tabs) {
+      this.try_injecting_single_widget(target, tab);
+    }
+  }
+
+  async try_injecting_single_widget(iframe, tab) {
+    if(!iframe) return false;
+    if(iframe.nodeName != "IFRAME") return false;
+
+    const character_dialog = iframe.parentNode;
+    if(!character_dialog) return false
+    if(!character_dialog.classList.contains("characterdialog")) return false;
+
+    const characterId = character_dialog.getAttribute(charIdAttribute);
+
+    if(tab.predicate) {
+      const char = R20.getCharacter(characterId);
+      if(!tab.predicate(char)) {
+        return;
+      }
     }
 
-    unselectSyntheticNavs(e) {
-        const navTabsRoot = e.target.parentNode.parentNode;
-        $(navTabsRoot).find("a[data-r20es-nav]").each((i, el) => {
-            el.parentNode.classList.remove("active");
-        });
-    }
+    //console.log("iframe is", iframe);
 
-    onClickNormalNavs(e) {
+    const wait_for_load = new Promise(ok => {
+      if(iframe.contentDocument.readyState == "complete") {
+        ok();
+        return;
+      }
 
-        this.unselectSyntheticNavs(e);
-        this.getWidgetTabRoots(e.target).each((i, obj) => {
-            obj.style.display = "none";
-        });
-    }
+      const listener = () => {
+        iframe.removeEventListener("load", listener);
+        ok();
+      }
 
-    navOnClick(e) {
-        const targetTabClass = e.target.getAttribute("data-tab");
+      iframe.addEventListener("load", listener);
+    });
 
-        const internalTabs = SheetTab._getInternalData();
-        const tab = internalTabs.tabsById[targetTabClass];
+    await wait_for_load;
 
-        const charRoot = $(e.target).closest(`[${charIdAttribute}]`)[0];
-        console.log(charRoot);
-        let tabInstance = null;
-        if(charRoot && charRoot.hasAttribute(charIdAttribute)) {
-            tabInstance = tab.getInstanceData(charRoot.getAttribute(charIdAttribute))
-        }
+    //console.log("iframe loaded");
 
-        if(tab && tab.onShow) {
-            tab.onShow(tabInstance);
-        }
+    let navTabsRoot = null
 
-        this.unselectSyntheticNavs(e);
-        e.target.parentNode.classList.add("active");
+    let body = null;
 
-        this.getWidgetTabRoots(e.target).each((i, obj) => {
-            obj.style.display = obj.classList.contains(targetTabClass)
-                ? "block"
-                : "none";
-        });
-    }
+    const retry = new Promise(ok => {
+      const retry_interval = 1000;
+      const check = () => {
 
-    tryInjectingWidget(target) {
-        const data = SheetTab._getInternalData();
+        body = iframe.contentDocument.body;
+        //console.log("body", body);
+        if(body) {
+          const dialog = body.querySelector("#dialog-window");
 
-        for (const tab of data.tabs) {
-            this.tryInjectingSingleWidget(target, tab);
-        }
-    }
+          //console.log("dialog", dialog);
 
-    tryInjectingSingleWidget(target, tab) {
+          if(dialog) {
+            navTabsRoot = dialog.querySelector(".nav-tabs");
 
-        if (!target) return false;
-        if (!target.className) return false;
-        if (!target.hasAttribute(charIdAttribute)) return false;
-        if (target.getElementsByClassName(tab.id).length > 0) return;
-        if ($(target).find(".charactereditor").length > 0) return;
+            //console.log("navTabsRoot", navTabsRoot);
 
-        const characterId = target.getAttribute(charIdAttribute);
-
-        if(tab.predicate) {
-
-            const char = R20.getCharacter(characterId);
-
-            if(!tab.predicate(char)) {
-                return;
+            if(navTabsRoot) {
+              ok();
+              return;
             }
+          }
         }
 
-        const nav = (
-            <li>
-                <a onClick={this.navOnClick} data-tab={tab.id} href="javascript:void(0);">
-                    {tab.name}
-                </a>
-            </li>
-        );
-        nav.firstElementChild.setAttribute(this.attribCustomNav, true);
+        setTimeout(check, retry_interval);
+      };
 
-        tab._addElem(nav);
-        target.firstElementChild.firstElementChild.appendChild(nav);
+      setTimeout(check, retry_interval);
+    });
 
+    const timeout = promiseWait(10000)
 
-        // register an event handler on the normal navbar tabs
-        // onClickNormalNavs will hide the custom stuff, state active state for the custom nav
-        const navTabsRoot = target.firstElementChild.firstElementChild;
-        $(navTabsRoot).find("a[data-tab]").each((i, el) => {
-            if (el.hasAttribute(this.attribCustomNav)) return;
-            if (el.hasAttribute(this.attribNavHasListener)) return;
+    await Promise.race([retry, timeout]);
 
-            el.setAttribute(this.attribNavHasListener, true);
-            el.addEventListener("click", this.onClickNormalNavs);
-            this.infectedNavs.push(el);
-        });
+    //console.log("navTabsRoot", navTabsRoot);
 
+    if(navTabsRoot) {
 
-        const tabInstanceData = tab.getInstanceData(characterId);
-
-        const tabroot = $(target.firstElementChild).find(".tab-content")[0];
-        const renderFxResult = tab.renderFx(tabInstanceData);
-
-        tabInstanceData.contentRoot = tabroot;
-        tabInstanceData.root = renderFxResult;
-
-        const widget = (
-            <div className={[this.tabStyle, tab.id, "tab-pane"]} style={{ display: "none" }}>
-                {renderFxResult}
-            </div>
-        );
-
-        tab._addElem(widget);
-        tabroot.appendChild(widget);
-
-        return true;
-    }
-
-    observerCallback(muts) {
-        for (var e of muts) {
-            for (const added of e.addedNodes) {
-                if (this.tryInjectingWidget(added)) {
-                    return;
-                }
-            }
-
-            if (this.tryInjectingWidget(e.target)) {
-                return;
-            }
+      {
+        const query_string = `[data-tab=${tab.id}]`
+        const query = navTabsRoot.querySelector(query_string);
+        //console.log(navTabsRoot, query_string, query);
+        if(query) {
+          return false;
         }
+      }
+
+      const nav = (
+        <li>
+          <a 
+            onClick={this.navOnClick} 
+            data-tab={tab.id} 
+            href="javascript:void(0);"
+            data-characterid={characterId}
+          >
+            {tab.name}
+          </a>
+        </li>
+      );
+
+      nav.firstElementChild.setAttribute(this.attribCustomNav, true);
+
+      tab._addElem(nav);
+
+      navTabsRoot.appendChild(nav);
+
+      // register an event handler on the normal navbar tabs
+      // onClickNormalNavs will hide the custom stuff, state active state for the custom nav
+      $(navTabsRoot).find("a[data-tab]").each((i, el) => {
+        if (el.hasAttribute(this.attribCustomNav)) return;
+        if (el.hasAttribute(this.attribNavHasListener)) return;
+
+        el.setAttribute(this.attribNavHasListener, true);
+        el.addEventListener("click", this.onClickNormalNavs);
+        this.infectedNavs.push(el);
+      });
+
+      const tabInstanceData = tab.getInstanceData(characterId);
+
+      const renderFxResult = tab.renderFx(tabInstanceData);
+
+      const tabroot = body.querySelector(".tab-content");
+      tabInstanceData.contentRoot = tabroot;
+      tabInstanceData.root = renderFxResult;
+
+      const widget = (
+        <div className={[this.tabStyle, tab.id, "tab-pane"]} style={{ display: "none" }}>
+          {renderFxResult}
+        </div>
+      );
+
+      tab._addElem(widget);
+      tabroot.appendChild(widget);
+    }
+    else {
+      console.error("SheetTab: Could not find navTabsRoot :(");
     }
 
-    rescan() {
-        const existingHeaders = document.querySelectorAll("div[data-characterid].dialog.characterdialog");
+    return true;
+  }
 
-        for (const header of existingHeaders) {
-            console.log(header);
-            this.tryInjectingWidget(header);
+  observerCallback(muts) {
+    for (var e of muts) {
+      for (const added of e.addedNodes) {
+        if(this.try_injecting_widget(added)) {
+          return;
         }
+      }
+
+      if(this.try_injecting_widget(e.target)) {
+        return;
+      }
+    }
+  }
+
+  rescan(tab) {
+    const existingHeaders = document.querySelectorAll("iframe");
+
+    for (const header of existingHeaders) {
+      this.try_injecting_single_widget(header, tab);
+    }
+  }
+
+  setup() {
+    this.observer = new MutationObserver(this.observerCallback);
+    this.observer.observe(document.body, { childList: true, subtree: true });
+
+    {
+      const existingHeaders = document.querySelectorAll("iframe");
+
+      for (const header of existingHeaders) {
+        this.try_injecting_widget(header);
+      }
     }
 
-    setup() {
-        this.rescan();
-        SheetTab._getInternalData().rescanFunc = this.rescan;
+    SheetTab._getInternalData().rescanFunc = this.rescan;
+  }
 
-        this.observer = new MutationObserver(this.observerCallback);
-        this.observer.observe(document.body, { childList: true, subtree: true });
+  dispose() {
+    SheetTab._getInternalData().rescanFunc = null;
+
+    const data = SheetTab._getInternalData();
+    for(const tab of data.tabs) {
+      tab.dispose();
     }
+    data.tabs = [];
 
-    dispose() {
-        SheetTab._getInternalData().rescanFunc = null;
+    this.infectedNavs.each(el => {
+      el.removeEventListener("click", this.onClickNormalNavs);
+      el.removeAttribute(this.attribNavHasListener);
+    });
 
-        const data = SheetTab._getInternalData();
-        for (const tab of data.tabs) {
-            tab.dispose();
-        }
-        data.tabs = [];
+    this.infectedNavs.length = 0;
 
-        this.infectedNavs.each(el => {
-            el.removeEventListener("click", this.onClickNormalNavs);
-            el.removeAttribute(this.attribNavHasListener);
-        });
-        this.infectedNavs.length = 0;
-
-        if (this.observer) this.observer.disconnect();
-    }
+    if (this.observer) this.observer.disconnect();
+  }
 }
 
 if (R20Module.canInstall()) new SheetTabApiModule().install();
