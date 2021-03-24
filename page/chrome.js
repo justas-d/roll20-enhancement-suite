7147652ac0f971923291ffff950d68e96d0eb358
Justas Dabrila <justasdabrila@gmail.com>
2021-03-24 21:09:32 +0200

    update page/chrome.js

--- page/chrome.js
+++ page/chrome.js
@@ -12,13 +12,27 @@ function Chrome() {
             <NavBar />
 
             <div className="container">
-                <h1>Chrome</h1>
+                <h1>Chrome Store</h1>
                 <hr style={{ marginTop: "0" }} />
 
+                <p>The extension was previously taken down from the Chrome store, but it is now back online.</p>
+                <p>To reduce the likelihood of this happening again, the extension is unlisted and available to Patrons of any tier.</p>
+
+                <a href="https://www.patreon.com/posts/extension-is-on-49155538" rel="noopener noreferrer">
+                  <img src="patreon.webp" alt="Become a Patron & Get link" style={{width: "217px", height: "51px"}}/>
+                </a>
+
+                <hr/>
+
+                <h2>Manual Installation</h2>
+
                 <p>As of 2019-09-30, the extension has been taken down from the Chrome Web Store in fear of copyright violation.</p>
-                <p>The original takedown statement can be read <a href="takedown.png">here</a></p>
+
+                <p>The original takedown statement can be read <a href="takedown.png">here.</a></p>
                 <p>Installing the extension is still possible but convoluted.</p>
                 <p>We recommend using Firefox to avoid the extra steps.</p>
+
+                <p><b>Beware that auto-updates will not be available while using this method.</b></p>
 
                 <ul>
                     <li>1. <a href={`https://github.com/justas-d/roll20-enhancement-suite/raw/master/page/r20es_${LATEST_CHROME_VERSION}_chrome.zip`}>Download the extension here</a> ({`last updated on ${CHROME_LAST_UPDATE_TIME}, version ${LATEST_CHROME_VERSION})`}</li>
@@ -30,7 +44,6 @@ function Chrome() {
                     <li>7. Make sure to not delete the extension folder as that will remove the extension.</li>
                 </ul>
 
-                <p>Beware that auto-updates will not be available while using this method.</p>
 
             </div>
 
