9c8a64a5af24cdf5e7df4c8d716ea928acbb425b
Justas Dabrila <justasdabrila@gmail.com>
2021-04-13 22:54:05 +0300

    LibreAudio: fix

--- src/modules/LibreAudio/Config.js
+++ src/modules/LibreAudio/Config.js
@@ -2,17 +2,17 @@ import MakeConfig from '../MakeConfig';
 import Category from '../Category';
 
 export default MakeConfig(__dirname, {
-    id: "libreAudio",
-    name: "Libre Audio",
-    description: "Allows creation and playback of tracks via their URL. Removes the 16 listener cap on My Audio Tracks. Refreshing the page is recommended after disabling/enabling this module in order to avoid issues.",
-    category: Category.freedom,
+  id: "libreAudio",
+  name: "Libre Audio",
+  description: "Allows creation and playback of tracks via their URL. Removes the 16 listener cap on My Audio Tracks. Refreshing the page is recommended after disabling/enabling this module in order to avoid issues.",
+  category: Category.freedom,
 
-    media: {
-        "libre_audio.png": "Add by URL button",
-    },
+  media: {
+    "libre_audio.png": "Add by URL button",
+  },
 
-    mods: [
-        /*
+  mods: [
+    /*
         { // play hook
             includes: "assets/app.js",
             find: `"My Audio"===r&&`,
@@ -24,17 +24,19 @@ export default MakeConfig(__dirname, {
             patch: `if(window.r20es && window.r20es.canPlaySound && window.r20es.playSound && window.r20es.canPlaySound(n)) { window.r20es.playSound(n) } else if("My Audio" === r) {`,
         },
         */
-        {
-            includes: "assets/app.js",
-            find: `"/audio_library/play/"+campaign_id+"/"+o.split("-")[0]`,
-            patch: `((window.r20es && window.r20es.canPlaySound && window.r20es.canPlaySound(n)) ? o : "/audio_library/play/"+campaign_id+"/"+o.split("-")[0])`
-        },
-        { // remove 16 player cap on my audio
 
-            includes: "assets/app.js",
-            find: `if(d20.Campaign.players.filter(function(e){return e.get("online")}).length>15)return t.stopAllTracks(),void(0===$("#myaudiocap").length&&($("#jukeboxwhatsplaying").after("<div id='myaudiocap' class='warning'>"+i18n("my_audio_cap","You've exceeded the maximum active game size for My Audio tracks. You may continue other audio track types. You can find more information on the <a href='https://wiki.roll20.net/Jukebox'>Roll20 Wiki</a>.")+"<span id='dismiss_myaudiocap' class='ui-icon ui-icon-closethick'>close</span></div>"),$("#dismiss_myaudiocap").on("click",function(){$("#myaudiocap").remove()})));`,
-            patch: ``,
-        },
-    ]
+    // play libreaudio sounds
+    {
+      includes: "assets/app.js",
+      find: `\`/audio_library/play/\${campaign_id}/\${n.split("-")[0]}\``,
+      patch: `((window.r20es && window.r20es.canPlaySound && window.r20es.canPlaySound(i)) ? n : >>R20ES_MOD_FIND>>)`,
+    },
+    {
+      includes: "assets/app.js",
+      find: `d20.Campaign.players.filter(e=>e.get("online")).length>15`,
+      patch: "false",
+    }
+
+  ]
 });
 
