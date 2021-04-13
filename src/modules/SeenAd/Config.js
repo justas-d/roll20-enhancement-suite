1ca81c83d4cae248c001be643f3889eee16a6eda
Justas Dabrila <justasdabrila@gmail.com>
2021-04-13 23:22:04 +0300

    Disable SeenAd

--- src/modules/SeenAd/Config.js
+++ src/modules/SeenAd/Config.js
@@ -2,7 +2,10 @@ export default {
     id: "seenadOverride",
     force: true,
 
-    includes: "/editor/startjs/",
-    find: "d20ext.showGoogleAd();",
-    patch: 'window.d20ext.seenad = !0, $("#loading-overlay").find("div").hide(), window.currentPlayer && d20.Campaign.pages.length > 0 && d20.Campaign.handlePlayerPageChanges(), void $.get("/editor/startping/true");'
+    // NOTE(justasd): the call to showGoogleAd seems to have been removed.
+    // Last checked: 2021-04-13
+
+    //includes: "/editor/startjs/",
+    //find: "d20ext.showGoogleAd();",
+    //patch: 'window.d20ext.seenad = !0, $("#loading-overlay").find("div").hide(), window.currentPlayer && d20.Campaign.pages.length > 0 && d20.Campaign.handlePlayerPageChanges(), void $.get("/editor/startping/true");'
 };
