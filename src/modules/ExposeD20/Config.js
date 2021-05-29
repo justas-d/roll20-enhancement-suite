a445e8e64286f85dd2b199d9c751d30f7e6052d5
Justas Dabrila <justasdabrila@gmail.com>
2021-05-29 13:43:20 +0300

    format ExposeD20

--- src/modules/ExposeD20/Config.js
+++ src/modules/ExposeD20/Config.js
@@ -1,10 +1,12 @@
 export default {
-    id: "exposeD20",
-    force: true,
+  id: "exposeD20",
+  force: true,
 
-    includes: "assets/app.js",
-    find: `var exports=exports||{},`,
-    patch: `window.d20 = d20; var exports=exports||{},`,
-
-    expectedPatchCount: 2
+  mods: [
+    {
+      includes: "assets/app.js",
+      find: `var exports=exports||{},`,
+      patch: `window.d20 = d20; var exports=exports||{},`,
+    },
+  ],
 };
