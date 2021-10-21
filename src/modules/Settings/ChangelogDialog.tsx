80f254a234d70617dcd0d11daa0eaf1b3f9632b9
Justas Dabrila <justasdabrila@gmail.com>
2021-10-21 20:26:07 +0300

    Make the 'ok' button on the changelog always appear at the bottom of the dialog

--- src/modules/Settings/ChangelogDialog.tsx
+++ src/modules/Settings/ChangelogDialog.tsx
@@ -5,43 +5,53 @@ import ChangelogWidget from "../ChangelogWidget";
 
 export default class ChangelogDialog extends DialogBase<null> {
 
-    constructor() {
-        super(null, {
-            maxWidth: "40%",
-            maxHeight: "85%"
-        });
-    }
+  constructor() {
+    super(null, {
+      maxWidth: "40%",
+      maxHeight: "85%"
+    });
+  }
 
-    public show() {
-        setTimeout(() => {
-            this.recenter();
-            this.getRoot().scrollTop = 0;
-        }, 100);
+  public show() {
+    setTimeout(() => {
+      this.recenter();
+      this.getRoot().scrollTop = 0;
+    }, 100);
 
-        this.internalShow();
-    };
-    protected render(): HTMLElement {
-        return (
-            <Dialog>
-                <DialogHeader style={{ textAlign: "center" }}>
-                    <h1>VTTES - Changelog</h1>
-                </DialogHeader>
+    this.internalShow();
+  };
+  protected render(): HTMLElement {
+    return (
+      <Dialog>
+        <DialogHeader style={{ textAlign: "center" }}>
+          <h1>VTTES - Changelog</h1>
+        </DialogHeader>
 
+        <DialogBody>
+          <ChangelogWidget listAllVersions={true}/>
+        </DialogBody>
 
-                <DialogBody>
-                    <ChangelogWidget listAllVersions={true}/>
-                </DialogBody>
-
-                <section style={{ margin: "20px" }}>
-                    <input
-                        className="btn"
-                        style={{ width: "100%", height: "auto", boxSizing: "border-box" }}
-                        type="button"
-                        onClick={this.close}
-                        value="OK" />
-                </section>
-
-            </Dialog> as any
-        );
-    }
+        <section style={{
+          position: "sticky",
+          padding: "20px",
+          bottom: 0,
+          top: 0,
+          left: 0,
+          backgroundColor: "rgb(253, 253, 253)",
+        }}>
+          <input
+            className="btn"
+            style={{
+              width: "100%", 
+              height: "auto", 
+              boxSizing: "border-box",
+            }}
+            type="button"
+            onClick={this.close}
+            value="OK" 
+          />
+        </section>
+      </Dialog> as any
+    );
+  }
 }
