530b1e863ffa33eb1c34b37792c685e3c6f41760
Justas Dabrila <justasdabrila@gmail.com>
2021-12-02 17:56:37 +0200

    Fox change id when duplicating

--- src/modules/ChangeIdWhenDuplicating/Config.ts
+++ src/modules/ChangeIdWhenDuplicating/Config.ts
@@ -11,7 +11,7 @@ export default MakeConfig(__dirname, {
       {
         includes: "vtt.bundle.js",
         find: `f.defaulttoken=n.model._blobcache.defaulttoken`,
-        patch: `f.defaulttoken = ((window.r20es && window.r20es.replaceIdOnDupe) ? window.r20es.replaceIdOnDupe(n, m) : n.model._blobcache.defaulttoken)`
+        patch: `f.defaulttoken = ((window.r20es && window.r20es.replaceIdOnDupe) ? window.r20es.replaceIdOnDupe(n, c) : n.model._blobcache.defaulttoken)`
       },
     ],
 })
