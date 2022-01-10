9c158a0146812230ae1e4855144aff4cc7e7ec9a
Justas Dabrila <justasdabrila@gmail.com>
2022-01-10 17:12:07 +0200

    formatting

--- src/tools/SheetRollTypeSetter/Config.ts
+++ src/tools/SheetRollTypeSetter/Config.ts
@@ -2,13 +2,13 @@ import {SheetRollTypeSetter} from "./index";
 import {ITool, IToolConfig} from "../ITool";
 
 const cfg: IToolConfig = {
-    id: "sheetRollTypeSetterTool",
-    description: "Allows bulk changing 5e OGL sheet roll queries, whisper types and damage auto roll settings.",
-    name: "Bulk 5e OGL Roll type setter",
+  id: "sheetRollTypeSetterTool",
+  description: "Allows bulk changing 5e OGL sheet roll query and whisper type settings.",
+  name: "Bulk 5e OGL Roll type setter",
 
-    factory: (): ITool => {
-        return new SheetRollTypeSetter();
-    },
+  factory: (): ITool => {
+    return new SheetRollTypeSetter();
+  },
 };
 
 export default cfg;
--- src/tools/SheetRollTypeSetter/index.tsx
+++ src/tools/SheetRollTypeSetter/index.tsx
@@ -3,122 +3,124 @@ import {DOM} from "../../utils/DOM";
 import {R20} from "../../utils/R20";
 
 interface IConfigurableAttribute {
-    label: string;
-    attributeName: string;
-    valueDisplayMap: { [value: string]: string };
+  label: string;
+  attribute_name: string;
+  value_display_map: Record<string, string>;
 }
 
-const attribs = [
-    {
-        label: "Advantage Roll Queries",
-        attributeName: "rtype",
-        valueDisplayMap: {
-            "{{always=1}} {{r2=[[@{d20}": "Always Roll Advantage",
-            "@{advantagetoggle}": "Advantage Toggle",
-            "{{query=1}} ?{Advantage?|Normal Roll,&#123&#123normal=1&#125&#125 &#123&#123r2=[[0d20|Advantage,&#123&#123advantage=1&#125&#125 &#123&#123r2=[[@{d20}|Disadvantage,&#123&#123disadvantage=1&#125&#125 &#123&#123r2=[[@{d20}}": "Query Advantage",
-            "{{normal=1}} {{r2=[[0d20": "Never Roll Advantage",
-        }
-    },
-
-    {
-        label: "Whisper Rolls To GM",
-        attributeName: "wtype",
-        valueDisplayMap: {
-            "": "Never Whisper Rolls",
-            "@{whispertoggle}": "Whisper Toggle",
-            "?{Whisper?|Public Roll,|Whisper Roll,/w gm }": "Query Whisper",
-            "/w gm ": "Always Whisper Rolls"
-        }
+const ALL_ATTRIB_DATAS: Array<IConfigurableAttribute> = [
+  {
+    label: "Advantage Roll Queries",
+    attribute_name: "rtype",
+    value_display_map: {
+      "{{always=1}} {{r2=[[@{d20}": "Always Roll Advantage",
+      "@{advantagetoggle}": "Advantage Toggle",
+      "{{query=1}} ?{Advantage?|Normal Roll,&#123&#123normal=1&#125&#125 &#123&#123r2=[[0d20|Advantage,&#123&#123advantage=1&#125&#125 &#123&#123r2=[[@{d20}|Disadvantage,&#123&#123disadvantage=1&#125&#125 &#123&#123r2=[[@{d20}}": "Query Advantage",
+      "{{normal=1}} {{r2=[[0d20": "Never Roll Advantage",
     }
+  },
+
+  {
+    label: "Whisper Rolls To GM",
+    attribute_name: "wtype",
+    value_display_map: {
+      "": "Never Whisper Rolls",
+      "@{whispertoggle}": "Whisper Toggle",
+      "?{Whisper?|Public Roll,|Whisper Roll,/w gm }": "Query Whisper",
+      "/w gm ": "Always Whisper Rolls"
+    }
+  }
 ];
 
 const onSetForAll = (e: Event) => {
-    e.stopPropagation();
-    const target = e.target as HTMLButtonElement;
-    target.disabled = true;
-    try {
+  e.stopPropagation();
 
-        const attribName = target.getAttribute("data-key");
-        if (!attribName) {
-            alert("Could not find attribute name.");
-            return;
-        }
+  const target = e.target as HTMLButtonElement;
+  target.disabled = true;
 
-        const attribData = attribs.find(a => a.attributeName === attribName);
-        if (!attribData) {
-            alert("Could not find attribute.");
-            return;
-        }
-
-        const selectElement = $(target.parentNode).find("select")[0] as HTMLSelectElement;
-        if (!selectElement) {
-            alert("Could not find select element.");
-            return;
-        }
-        const selectValue = selectElement.value;
-
-        const chars = R20.getAllCharacters();
-
-        const mod = {
-            current: selectValue
-        };
-
-        for (const char of chars) {
-
-            let attrib: Roll20.CharacterSheetAttribute | null = null;
-            for (const tempAttrib of char.attribs.models) {
-                if (tempAttrib.attributes.name !== attribData.attributeName) continue;
-                attrib = tempAttrib;
-                break;
-            }
-
-            if (!attrib) {
-                attrib = char.attribs.create({
-                    name: attribData.attributeName
-                });
-            }
-            
-            attrib.save(mod);
-        }
-
-    } catch(err) {
-        console.error(err);
-        alert(`Error: ${err}`);
-    } finally {
-        target.disabled =false;
-        target.innerText = `${setForAllText} (done!)`;
+  try {
+    const attrib_name = target.getAttribute("data-key");
+    if(!attrib_name) {
+      alert("Could not find attribute name.");
+      return;
     }
+
+    const attrib_data = ALL_ATTRIB_DATAS.find(a => a.attribute_name === attrib_name);
+    if(!attrib_data) {
+      alert("Could not find attribute.");
+      return;
+    }
+
+    const select_element = $(target.parentNode).find("select")[0] as HTMLSelectElement;
+    if(!select_element) {
+      alert("Could not find select element.");
+      return;
+    }
+
+    const select_value = select_element.value;
+
+    const chars = R20.getAllCharacters();
+
+    const mod = { current: select_value };
+
+    for(const char of chars) {
+      let attrib: Roll20.CharacterSheetAttribute | null = null;
+
+      for(const temp_attrib of char.attribs.models) {
+
+        if(temp_attrib.attributes.name !== attrib_data.attribute_name) {
+          continue;
+        }
+
+        attrib = temp_attrib;
+        break;
+      }
+
+      if(!attrib) {
+        attrib = char.attribs.create({
+          name: attrib_data.attribute_name
+        });
+      }
+      
+      attrib.save(mod);
+    }
+  } 
+  catch(err) {
+    console.error(err);
+    alert(`Error: ${err}`);
+  } 
+  finally {
+    target.disabled = false;
+    target.innerText = `Set for all (done!)`;
+  }
 };
 
-const setForAllText = "Set for all";
-
 const generateSelect = (cfg: IConfigurableAttribute) => {
-    const options = [];
+  const options = [];
 
-    for (const key in cfg.valueDisplayMap) {
-        options.push(<option value={key}>{cfg.valueDisplayMap[key]}</option>);
-    }
+  for (const key in cfg.value_display_map) {
+    options.push(<option value={key}>{cfg.value_display_map[key]}</option>);
+  }
 
-    return (
-        <div style={{display: "grid", gridTemplateColumns: "2fr 3fr 1fr"}}>
-            <span>{cfg.label}</span>
+  return (
+    <div style={{display: "grid", gridTemplateColumns: "2fr 3fr 1fr"}}>
+      <span>{cfg.label}</span>
 
-            <select>
-                {options}
-            </select>
+      <select>
+        {options}
+      </select>
 
-            <button data-key={cfg.attributeName} onClick={onSetForAll} className="btn">{setForAllText}</button>
-        </div>
-    )
+      <button data-key={cfg.attribute_name} onClick={onSetForAll} className="btn">Set for all</button>
+    </div>
+  )
 };
 
 export class SheetRollTypeSetter implements ITool {
-    public show(): HTMLElement {
-        return (
-
-            <div>
-                {attribs.map(generateSelect)}
-            </div>
-        );
-    }
+  public show(): HTMLElement {
+    return (
+      <div>
+        {ALL_ATTRIB_DATAS.map(generateSelect)}
+      </div>
+    );
+  }
 }
