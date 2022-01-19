512a8f23a7994fdeae5a7df42c9003add1de7d71
Justas Dabrila <justasdabrila@gmail.com>
2022-01-19 21:58:46 +0200

    Fix animated backgrounds

--- src/modules/AnimatedBackgroundLayer/Config.ts
+++ src/modules/AnimatedBackgroundLayer/Config.ts
@@ -35,18 +35,17 @@ export default <VTTES.Module_Config> {
   mods: [
     {
       includes: "vtt.bundle.js",
-      find: `updateCanvasZoom:()=>d20.engine.canvasZoom=G.canvasZoom`,
-
+      find: `updateCanvasZoom:()=>d20.engine.canvasZoom=te.canvasZoom`,
 
       patch: `updateCanvasZoom:()=> {
         if(window.r20es && window.r20es.onZoomChange) {
-          window.r20es.onZoomChange(D);
+          window.r20es.onZoomChange(B);
         }
-        d20.engine.canvasZoom = G.canvasZoom;
+        d20.engine.canvasZoom = te.canvasZoom;
       }`,
 
       stability_checks: [
-        `d20.engine.setZoom=(D,U,q)`,
+        `d20.engine.setZoom=(B,q,K)`,
       ],
     }
   ]
