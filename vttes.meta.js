1c17935e07c353f8d0f22c0756f56ebfe2dcd809
Justas Dabrila <justasdabrila@gmail.com>
2022-01-19 11:22:39 +0200

    script

--- vttes.meta.js
+++ vttes.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VTT Enhancement Suite
 // @namespace    https://justas-d.github.io/
-// @version      1.26.9
+// @version      1.26.10
 // @description  aka R20ES. Provides quality-of-life and workflow speed improvements to Roll20.
 // @author       @Justas_Dabrila
 // @updateURL    https://justas-d.github.io/roll20-enhancement-suite/vttes.meta.js
