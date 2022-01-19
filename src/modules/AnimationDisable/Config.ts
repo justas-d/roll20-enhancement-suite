758cf596af320084c36d8e4f8b86a2a400002167
Justas Dabrila <justasdabrila@gmail.com>
2022-01-19 21:39:14 +0200

    Fix animation disable

--- src/modules/AnimationDisable/Config.ts
+++ src/modules/AnimationDisable/Config.ts
@@ -16,29 +16,37 @@ export default <VTTES.Module_Config> {
 
     { // radial button proportionally timed animation
       includes: "vtt.bundle.js",
-      find: `setTimeout(function(){$(c).addClass("open"),p.find(".button div.hasnumber").textfill(20)},l*30),l++`,
+
+      // NOTE(justasd): search for * 30)
+      // 2022-01-19
+      find: `setTimeout(function(){$(t).addClass("open"),m.find(".button div.hasnumber").textfill(20)},g*30),g++`,
 
       patch: `;
-      if(window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disableRadial")) { $(c).addClass("open");p.find(".button div.hasnumber").textfill(20);}
+      if(window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disableRadial")) { $(t).addClass("open");m.find(".button div.hasnumber").textfill(20);}
       else { >>R20ES_MOD_FIND>>; }
       `,
     },
 
     { // radial final
       includes: "vtt.bundle.js",
-      find: `setTimeout(function(){p.find(".button").addClass("animcomplete")},250)`,
+
+      // NOTE(justasd): search for * 30)
+      // 2022-01-19
+      find: `setTimeout(function(){m.find(".button").addClass("animcomplete")},250)`,
 
       patch: `1;
-      if(window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disableRadial")) { p.find(".button").addClass("animcomplete");}
+      if(window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disableRadial")) { m.find(".button").addClass("animcomplete");}
       else { >>R20ES_MOD_FIND>>; }`,
     },
 
     // toolbar anim
     {
       includes: "vtt.bundle.js",
-      find: `h.animate({top:"-1px"},300).removeClass("closed"),`,
+      // NOTE(justasd): search for pagetoolbar.noClosing
+      // 2022-01-19
+      find: `l.animate({top:"-1px"},300).removeClass("closed"),`,
 
-      patch: `h.animate({top:"-1px"},
+      patch: `l.animate({top:"-1px"},
     (window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disablePageToolbar")) ? 1 : 300,
   ).removeClass("closed"),`,
     },
@@ -46,11 +54,14 @@ export default <VTTES.Module_Config> {
     // toolbar anim
     {
       includes: "vtt.bundle.js",
-      find: `h.animate({top:\`-\${h.height()}px\`},300,()=>{h.addClass("closed"),$("#page-toolbar .pages").hide(),_.delay(()=>{$("#page-toolbar .pages input:text").trigger("blur")})})`,
+      // NOTE(justasd): search for pagetoolbar.noClosing
+      // 2022-01-19
+      find: `l.animate({top:\`-\${l.height()}px\`},300,()=>{l.addClass("closed"),$("#page-toolbar .pages").hide(),_.delay(()=>{$("#page-toolbar .pages input:text").trigger("blur")})})`,
 
-      patch: `h.animate({top:"-"+h.height()+"px"},
+
+      patch: `l.animate({top:"-"+l.height()+"px"},
     (window.r20es && window.r20es.shouldDoCustomAnim && window.r20es.shouldDoCustomAnim("disablePageToolbar")) ? 1 : 300,
-    function(){h.addClass("closed"),$("#page-toolbar .pages").hide(),_.delay(function(){$("#page-toolbar .pages input").trigger("blur")})})`,
+    function(){l.addClass("closed"),$("#page-toolbar .pages").hide(),_.delay(function(){$("#page-toolbar .pages input").trigger("blur")})})`,
     }
   ],
 
