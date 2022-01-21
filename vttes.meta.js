13792215ae39520f0001c9d37bc3ad61fef886cc
Justas Dabrila <justasdabrila@gmail.com>
2022-01-21 17:44:49 +0200

    script

--- vttes.meta.js
+++ vttes.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VTT Enhancement Suite
 // @namespace    https://justas-d.github.io/
-// @version      1.26.11
+// @version      1.26.12
 // @description  aka R20ES. Provides quality-of-life and workflow speed improvements to Roll20.
 // @author       @Justas_Dabrila
 // @updateURL    https://justas-d.github.io/roll20-enhancement-suite/vttes.meta.js
@@ -26,9 +26,6 @@
 // @webRequest [{"selector":{"include":"*://app.roll20.net/v2/js/patience.js","exclude":"*://app.roll20.net/v2/js/patience.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/editor/startjs/?timestamp*","exclude":"*://app.roll20.net/editor/startjs/?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/d20/loading.js?v=11","exclude":"*://app.roll20.net/js/d20/loading.js?n=11&v=11"},"action":"cancel"}]
-// @webRequest [{"selector":{"include":"*://app.roll20.net/assets/firebase.8.8.1.js","exclude":"*://app.roll20.net/assets/firebase.8.8.1.js?n*"},"action":"cancel"}]
-// @webRequest [{"selector":{"include":"*://cdn.roll20.net/production/base.js","exclude":"*://cdn.roll20.net/production/base.js?n*"},"action":"cancel"}]
-// @webRequest [{"selector":{"include":"*://cdn.roll20.net/production/app.js","exclude":"*://cdn.roll20.net/production/app.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://cdn.roll20.net/production/vtt.bundle.js","exclude":"*://cdn.roll20.net/production/vtt.bundle.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/tutorial_tips.js","exclude":"*://app.roll20.net/js/tutorial_tips.js?n*"},"action":"cancel"}]
 // ==/UserScript==
