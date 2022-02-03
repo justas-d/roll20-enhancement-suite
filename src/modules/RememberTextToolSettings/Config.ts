4e9a1416ca4734424ea935d4aedcacd7c8821a84
Justas Dabrila <justasdabrila@gmail.com>
2022-02-03 11:43:03 +0200

    Fix remember text tool settings

--- src/modules/RememberTextToolSettings/Config.ts
+++ src/modules/RememberTextToolSettings/Config.ts
@@ -40,7 +40,7 @@ export default <VTTES.Module_Config> {
   mods: [
     {
       includes: "vtt.bundle.js",
-      find: `p.type=="text"&&($("#font-size").val(p.model.get("font_size"))`,
+      find: `g.type=="text"&&($("#font-size").val(g.model.get("font_size"))`,
       patch: `if((window.r20es && window.r20es.copyTextSettingsOnSelect) || !window.r20es) >>R20ES_MOD_FIND>>`,
     },
 
@@ -48,7 +48,7 @@ export default <VTTES.Module_Config> {
     // 2022-01-19
     {
       includes: "vtt.bundle.js",
-      find: `$("#font-size").val(ge).trigger("keyup"),$("#font-color").val(he).trigger("change-silent"),$("#font-family").val(se);`,
+      find: `$("#font-size").val(se).trigger("keyup"),$("#font-color").val(me).trigger("change-silent"),$("#font-family").val(ce);`,
       patch: `if((window.r20es && window.r20es.copyTextSettingsOnSelect) || !window.r20es) { >>R20ES_MOD_FIND>> }`,
     }
   ]
