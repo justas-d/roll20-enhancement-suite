b8942674ff3d23787fa1e178abc824ff13d16c31
Justas Dabrila <justasdabrila@gmail.com>
2022-02-03 11:43:50 +0200

    Make the disable player drawings module not be enabled by default

--- src/modules/DisablePlayerDrawings/Config.ts
+++ src/modules/DisablePlayerDrawings/Config.ts
@@ -5,11 +5,15 @@ export default <VTTES.Module_Config> {
 
   id: "disablePlayerDrawing",
   name: "Disable Player Drawing",
-  description: "Disables the drawing/text tools for certain players. Only the GM needs to have the extension installed for this to work.",
+  description: "Disables the drawing/text tools for certain players. Only the GM needs to have the extension installed for this to work. WARNING: This will load all pages in your game and may cause performance issues or loading slowdowns.",
   category: VTTES.Module_Category.canvas,
   gmOnly: true,
 
   media: {
     "disable_drawing.png": "Disable/Enable drawing permission button"
+  },
+
+  config: {
+    enabled: false,
   }
 };
