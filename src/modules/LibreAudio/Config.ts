8ffb04bab0e00ed0e92c2b5dcdc51305576f4146
Justas Dabrila <justasdabrila@gmail.com>
2022-02-12 12:22:18 +0200

    Fix libre audio

--- src/modules/LibreAudio/Config.ts
+++ src/modules/LibreAudio/Config.ts
@@ -17,17 +17,16 @@ export default <VTTES.Module_Config> {
     // play libreaudio sounds
     {
       includes: "vtt.bundle.js",
-      find: `\`/audio_library/play/\${campaign_id}/\${J.split("-")[0]}\``,
-
-      patch: `((window.r20es && window.r20es.canPlaySound && window.r20es.canPlaySound(U)) ? J : >>R20ES_MOD_FIND>>)`,
+      find: `\`/audio_library/play/\${campaign_id}/\${K.split("-")[0]}\``,
+      patch: `((window.r20es && window.r20es.canPlaySound && window.r20es.canPlaySound(z)) ? K : >>R20ES_MOD_FIND>>)`,
 
       stability_checks: [
-        `if(U.get("playing")&&U.get("softstop")==!1)`,
+        `if(z.get("playing")&&z.get("softstop")==!1)`,
       ],
     },
     {
       includes: "vtt.bundle.js",
-      find: `d20.Campaign.players.filter(K=>K.get("online")).length>15`,
+      find: `d20.Campaign.players.filter(J=>J.get("online")).length>15`,
       patch: "false",
     }
   ]
