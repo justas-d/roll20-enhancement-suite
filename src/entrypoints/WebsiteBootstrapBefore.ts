5ba4a0e272fa0cc3b18af8d040f43e894bc5ca89
Justas Dabrila <justasdabrila@gmail.com>
2022-05-25 12:28:54 +0300

    Remove the Sprig workaround.
    They seem to have stopped using Sprig.

--- src/entrypoints/WebsiteBootstrapBefore.ts
+++ src/entrypoints/WebsiteBootstrapBefore.ts
@@ -1,12 +1,2 @@
 window.enhancementSuiteEnabled = true;
 console.log(`Set window.enhancementSuiteEnabled to ${window.enhancementSuiteEnabled}`);
-
-// @SprigWorkaround
-window.sprig_safe_trampoline = (...args) => {
-  try {
-    window.Sprig(...args);
-  } catch(ex) {
-  }
-}
-console.log("Installed sprig_safe_trampoline");
-
