30d8d0899b857b77976a98e3c2ced205e66b26bc
Justas Dabrila <justasdabrila@gmail.com>
2022-07-13 10:45:33 +0300

    Webpack publicPath throw workaround for Chrome

--- src/modules/ExposeD20/Config.ts
+++ src/modules/ExposeD20/Config.ts
@@ -16,6 +16,22 @@ export default <VTTES.Module_Config> {
           find: `var exports=exports||{},`,
           replace: `window.d20 = d20; var exports=exports||{},`,
         },
+        {
+          // NOTE(justasd): This seems to be part of some kind of Webpack code in vtt.bundle.js.
+          // If we don't remove the exception throw, it gets thrown on Chrome.
+          // This was starting to cause issues starting starting on 2022-07-14 but could have been
+          // there earlier. Definitely wasn't there around 2022-05-15
+          //
+          // Anyway, if we yeet the exception throw, things seem to be working fine. It doesn't
+          // seem that the function that contains the throw even does any useful work inside of it.
+          // On top of that, this seemed to cause no issues in testing.
+          //
+          // So yeeting this seems fine.
+          //
+          // 2022-07-13
+          find: `if(!v)throw new Error("Automatic publicPath is not supported in this browser");`,
+          replace: `if(!v)v="";`,
+        },
       ],
     },
   ],
