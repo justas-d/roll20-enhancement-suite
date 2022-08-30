5f3ae5eb641447069527addf2f12f970b383694b
Justas Dabrila <justasdabrila@gmail.com>
2022-08-30 16:37:42 +0300

    Disable scale token name module

--- src/modules/ScaleTokenNamesBySize/Config.ts
+++ src/modules/ScaleTokenNamesBySize/Config.ts
@@ -1,73 +1,73 @@
-import TransformDirname from '../../utils/TransformDirname'
-
-export default <VTTES.Module_Config> {
-  filename: TransformDirname(__dirname),
-  id: "scaleTokenNamesBySize",
-  name: `Scale Token Names By Size`,
-  description: `Scales canvas token names proportionally by their size. Smaller tokens display smaller names while larger ones have large names.`,
-  category: VTTES.Module_Category.canvas,
-  gmOnly: false,
-
-  mods: [
-    {
-      includes: "vtt.bundle.js",
-      find_replace: [
-        {
-          //find: `render:function(c,o){`,
-          //replace: `render:function(c,o){debugger;`,
-          
-          // <TokenProperties
-          //find: `function b(T,M,z){`,
-          //replace: `function b(T,M,z){console.log(T,M,z);`,
-
-          //find: `}=Y.div;X.left=`,
-          //replace: `}=Y.div;(window.r20es && window.r20es.doNameplateScaling && window.r20es.doNameplateScaling(Y));X.left=`,
-        },
-      ],
-    },
-
-    {
-      includes: "vtt.bundle.js",
-
-      // NOTE(justasd): search for _drawNameplate: function(d)
-      // 2022-01-19
-      stencils: [
-        // d.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),d.fillStyle="rgb(0,0,0)",d.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+m+this._nameplate_data.padding)`,
-        
-        /*
-        {
-          find: [ `.fillStyle="rgba(255,255,255,0.5)",`,1,`.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),`,1,`.fillStyle="rgb(0,0,0)",`,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)` ],
-          replace: [
-`.fillStyle="rgba(255,255,255,0.5)",
-window.r20es && window.r20es.prepNameplateBack && window.r20es.prepNameplateBack(this, `,1,`), `,1,`.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),`,1,`.fillStyle="rgb(0,0,0)",
-window.r20es && window.r20es.prepNameplateText && window.r20es.prepNameplateText(this, `,1,`), `,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)`
-          ],
-        }
-        */
-      ],
-    },
-  ],
-
-  configView: {
-    widthThreshold: {
-      display: "The unit width of a token. Nameplates will not be scaled when a token has this width.",
-      type: VTTES.Config_View_Type.Number
-    },
-
-    scaleIfLarger: {
-      display: "Scale nameplate if token width is larger than the unit width.",
-      type: VTTES.Config_View_Type.Checkbox,
-    },
-
-    scaleIfSmaller: {
-      display: "Scale nameplate if token is smaller than the unit width.",
-      type: VTTES.Config_View_Type.Checkbox,
-    }
-  },
-
-  config: {
-    widthThreshold: 70,
-    scaleIfLarger: false,
-    scaleIfSmaller: true,
-  }
-};
+//import TransformDirname from '../../utils/TransformDirname'
+//
+//export default <VTTES.Module_Config> {
+//  filename: TransformDirname(__dirname),
+//  id: "scaleTokenNamesBySize",
+//  name: `Scale Token Names By Size`,
+//  description: `Scales canvas token names proportionally by their size. Smaller tokens display smaller names while larger ones have large names.`,
+//  category: VTTES.Module_Category.canvas,
+//  gmOnly: false,
+//
+//  mods: [
+//    {
+//      includes: "vtt.bundle.js",
+//      find_replace: [
+//        {
+//          //find: `render:function(c,o){`,
+//          //replace: `render:function(c,o){debugger;`,
+//          
+//          // <TokenProperties
+//          //find: `function b(T,M,z){`,
+//          //replace: `function b(T,M,z){console.log(T,M,z);`,
+//
+//          //find: `}=Y.div;X.left=`,
+//          //replace: `}=Y.div;(window.r20es && window.r20es.doNameplateScaling && window.r20es.doNameplateScaling(Y));X.left=`,
+//        },
+//      ],
+//    },
+//
+//    {
+//      includes: "vtt.bundle.js",
+//
+//      // NOTE(justasd): search for _drawNameplate: function(d)
+//      // 2022-01-19
+//      stencils: [
+//        // d.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),d.fillStyle="rgb(0,0,0)",d.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+m+this._nameplate_data.padding)`,
+//        
+//        /*
+//        {
+//          find: [ `.fillStyle="rgba(255,255,255,0.5)",`,1,`.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),`,1,`.fillStyle="rgb(0,0,0)",`,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)` ],
+//          replace: [
+//`.fillStyle="rgba(255,255,255,0.5)",
+//window.r20es && window.r20es.prepNameplateBack && window.r20es.prepNameplateBack(this, `,1,`), `,1,`.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),`,1,`.fillStyle="rgb(0,0,0)",
+//window.r20es && window.r20es.prepNameplateText && window.r20es.prepNameplateText(this, `,1,`), `,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)`
+//          ],
+//        }
+//        */
+//      ],
+//    },
+//  ],
+//
+//  configView: {
+//    widthThreshold: {
+//      display: "The unit width of a token. Nameplates will not be scaled when a token has this width.",
+//      type: VTTES.Config_View_Type.Number
+//    },
+//
+//    scaleIfLarger: {
+//      display: "Scale nameplate if token width is larger than the unit width.",
+//      type: VTTES.Config_View_Type.Checkbox,
+//    },
+//
+//    scaleIfSmaller: {
+//      display: "Scale nameplate if token is smaller than the unit width.",
+//      type: VTTES.Config_View_Type.Checkbox,
+//    }
+//  },
+//
+//  config: {
+//    widthThreshold: 70,
+//    scaleIfLarger: false,
+//    scaleIfSmaller: true,
+//  }
+//};
--- src/modules/ScaleTokenNamesBySize/Module.ts
+++ src/modules/ScaleTokenNamesBySize/Module.ts
@@ -1,103 +1,103 @@
-import {R20Module} from "../../utils/R20Module"
-import {R20} from "../../utils/R20";
-
-interface VueInfo {
-  div: HTMLDivElement;
-  graphic: Roll20.Token;
-};
-
-class ScaleTokenNamesBySizeModule extends R20Module.OnAppLoadBase {
-  public constructor() {
-    super(__dirname);
-  }
-
-  clear_scales = () => {
-    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
-    for(const el of els) {
-      el.style.transform = "";
-    }
-  }
-
-  rerender_scales = () => {
-    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
-    for(const el of els) {
-      // ...
-    }
-  }
-
-  set_scale = (token: Roll20.Token, nameplate_div: HTMLDivElement) => {
-    const scale = this.getScale(token);
-    console.log(scale);
-    nameplate_div.style.transform = `scale(${scale})`;
-  }
-
-
-  private onSettingChange(name: string, oldVal: any, newVal: any) {
-    this.rerender_scales();
-  }
-
-  doNameplateScaling = (data: VueInfo) => {
-    const nameplate_div = data.div.querySelector("div.nameplate") as HTMLDivElement;
-
-    if(typeof(nameplate_div) === "object") {
-      this.set_scale(data.graphic, nameplate_div);
-    }
-  }
-
-  getScale = (token: Roll20.Token) => {
-    const cfg = this.getHook().config;
-    let scale = token.attributes.width / cfg.widthThreshold;
-
-    if (scale < 1 && !cfg.scaleIfSmaller) scale = 1;
-    if (scale > 1 && !cfg.scaleIfLarger) scale = 1;
-
-    return scale;
-  };
-
-  /*
-  private prepNameplateBack = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
-    try {
-      const cfg = this.getHook().config;
-      let scale = this.getScale(token);
-
-      const scaledFontSize = token._nameplate_data.font_size * scale;
-      e.font = `bold ${scaledFontSize}px Arial`;
-
-      const width = e.measureText(token._nameplate_data.name).width;
-      const t = token.height / 2;
-      const n = e.measureText("M").width;
-
-      token._nameplate_data.position = [-width / 2 - token._nameplate_data.padding, t + token._nameplate_data.vertical_offset];
-      token._nameplate_data.size = [width + 2 * token._nameplate_data.padding, n + 2 * token._nameplate_data.padding];
-    }
-    catch (err) {
-      console.error(err);
-    }
-  };
-
-  private prepNameplateText = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
-    try {
-      let scale = this.getScale(token) - 1;
-      token._nameplate_data.position[1] += token._nameplate_data.font_size * scale;
-    }
-    catch (err) {
-      console.error(err);
-    }
-  };
-  */
-
-  public setup() {
-    window.r20es.doNameplateScaling = this.doNameplateScaling;
-    this.rerender_scales();
-  }
-
-  public dispose() {
-    window.r20es.doNameplateScaling = undefined;
-    this.clear_scales();
-  }
-}
-
-export default () => {
-  new ScaleTokenNamesBySizeModule().install();
-};
-
+//import {R20Module} from "../../utils/R20Module"
+//import {R20} from "../../utils/R20";
+//
+//interface VueInfo {
+//  div: HTMLDivElement;
+//  graphic: Roll20.Token;
+//};
+//
+//class ScaleTokenNamesBySizeModule extends R20Module.OnAppLoadBase {
+//  public constructor() {
+//    super(__dirname);
+//  }
+//
+//  clear_scales = () => {
+//    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
+//    for(const el of els) {
+//      el.style.transform = "";
+//    }
+//  }
+//
+//  rerender_scales = () => {
+//    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
+//    for(const el of els) {
+//      // ...
+//    }
+//  }
+//
+//  set_scale = (token: Roll20.Token, nameplate_div: HTMLDivElement) => {
+//    const scale = this.getScale(token);
+//    console.log(scale);
+//    nameplate_div.style.transform = `scale(${scale})`;
+//  }
+//
+//
+//  private onSettingChange(name: string, oldVal: any, newVal: any) {
+//    this.rerender_scales();
+//  }
+//
+//  doNameplateScaling = (data: VueInfo) => {
+//    const nameplate_div = data.div.querySelector("div.nameplate") as HTMLDivElement;
+//
+//    if(typeof(nameplate_div) === "object") {
+//      this.set_scale(data.graphic, nameplate_div);
+//    }
+//  }
+//
+//  getScale = (token: Roll20.Token) => {
+//    const cfg = this.getHook().config;
+//    let scale = token.attributes.width / cfg.widthThreshold;
+//
+//    if (scale < 1 && !cfg.scaleIfSmaller) scale = 1;
+//    if (scale > 1 && !cfg.scaleIfLarger) scale = 1;
+//
+//    return scale;
+//  };
+//
+//  /*
+//  private prepNameplateBack = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
+//    try {
+//      const cfg = this.getHook().config;
+//      let scale = this.getScale(token);
+//
+//      const scaledFontSize = token._nameplate_data.font_size * scale;
+//      e.font = `bold ${scaledFontSize}px Arial`;
+//
+//      const width = e.measureText(token._nameplate_data.name).width;
+//      const t = token.height / 2;
+//      const n = e.measureText("M").width;
+//
+//      token._nameplate_data.position = [-width / 2 - token._nameplate_data.padding, t + token._nameplate_data.vertical_offset];
+//      token._nameplate_data.size = [width + 2 * token._nameplate_data.padding, n + 2 * token._nameplate_data.padding];
+//    }
+//    catch (err) {
+//      console.error(err);
+//    }
+//  };
+//
+//  private prepNameplateText = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
+//    try {
+//      let scale = this.getScale(token) - 1;
+//      token._nameplate_data.position[1] += token._nameplate_data.font_size * scale;
+//    }
+//    catch (err) {
+//      console.error(err);
+//    }
+//  };
+//  */
+//
+//  public setup() {
+//    window.r20es.doNameplateScaling = this.doNameplateScaling;
+//    this.rerender_scales();
+//  }
+//
+//  public dispose() {
+//    window.r20es.doNameplateScaling = undefined;
+//    this.clear_scales();
+//  }
+//}
+//
+//export default () => {
+//  new ScaleTokenNamesBySizeModule().install();
+//};
+//
