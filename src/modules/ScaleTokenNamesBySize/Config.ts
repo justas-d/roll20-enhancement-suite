41d9a1e56dbb8ac24d2c523ffd6783eb3a8b09e3
Justas Dabrila <justasdabrila@gmail.com>
2022-08-30 16:34:35 +0300

    experiments

--- src/modules/ScaleTokenNamesBySize/Config.ts
+++ src/modules/ScaleTokenNamesBySize/Config.ts
@@ -11,11 +11,30 @@ export default <VTTES.Module_Config> {
   mods: [
     {
       includes: "vtt.bundle.js",
+      find_replace: [
+        {
+          //find: `render:function(c,o){`,
+          //replace: `render:function(c,o){debugger;`,
+          
+          // <TokenProperties
+          //find: `function b(T,M,z){`,
+          //replace: `function b(T,M,z){console.log(T,M,z);`,
+
+          //find: `}=Y.div;X.left=`,
+          //replace: `}=Y.div;(window.r20es && window.r20es.doNameplateScaling && window.r20es.doNameplateScaling(Y));X.left=`,
+        },
+      ],
+    },
+
+    {
+      includes: "vtt.bundle.js",
 
       // NOTE(justasd): search for _drawNameplate: function(d)
       // 2022-01-19
       stencils: [
         // d.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),d.fillStyle="rgb(0,0,0)",d.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+m+this._nameplate_data.padding)`,
+        
+        /*
         {
           find: [ `.fillStyle="rgba(255,255,255,0.5)",`,1,`.fillRect(...this._nameplate_data.position,...this._nameplate_data.size),`,1,`.fillStyle="rgb(0,0,0)",`,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)` ],
           replace: [
@@ -24,6 +43,7 @@ window.r20es && window.r20es.prepNameplateBack && window.r20es.prepNameplateBack
 window.r20es && window.r20es.prepNameplateText && window.r20es.prepNameplateText(this, `,1,`), `,1,`.fillText(this._nameplate_data.name,0,this._nameplate_data.position[1]+`,2,`+this._nameplate_data.padding)`
           ],
         }
+        */
       ],
     },
   ],
--- src/modules/ScaleTokenNamesBySize/Module.ts
+++ src/modules/ScaleTokenNamesBySize/Module.ts
@@ -1,67 +1,100 @@
 import {R20Module} from "../../utils/R20Module"
 import {R20} from "../../utils/R20";
 
+interface VueInfo {
+  div: HTMLDivElement;
+  graphic: Roll20.Token;
+};
+
 class ScaleTokenNamesBySizeModule extends R20Module.OnAppLoadBase {
-    public constructor() {
-        super(__dirname);
+  public constructor() {
+    super(__dirname);
+  }
+
+  clear_scales = () => {
+    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
+    for(const el of els) {
+      el.style.transform = "";
     }
+  }
 
-    private onSettingChange(name: string, oldVal: any, newVal: any) {
-        R20.renderAll();
+  rerender_scales = () => {
+    var els = document.body.querySelectorAll("div.nameplate-container .nameplate") as any as Array<HTMLElement>;
+    for(const el of els) {
+      // ...
     }
+  }
 
-    getScale = (token: Roll20.CanvasObject) => {
-        const cfg = this.getHook().config;
-        let scale = token.width / cfg.widthThreshold;
+  set_scale = (token: Roll20.Token, nameplate_div: HTMLDivElement) => {
+    const scale = this.getScale(token);
+    console.log(scale);
+    nameplate_div.style.transform = `scale(${scale})`;
+  }
 
-        if (scale < 1 && !cfg.scaleIfSmaller) scale = 1;
-        if (scale > 1 && !cfg.scaleIfLarger) scale = 1;
 
-        return scale;
-    };
+  private onSettingChange(name: string, oldVal: any, newVal: any) {
+    this.rerender_scales();
+  }
 
-    private prepNameplateBack = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
-        try {
-            const cfg = this.getHook().config;
-            let scale = this.getScale(token);
+  doNameplateScaling = (data: VueInfo) => {
+    const nameplate_div = data.div.querySelector("div.nameplate") as HTMLDivElement;
 
-            const scaledFontSize = token._nameplate_data.font_size * scale;
-            e.font = `bold ${scaledFontSize}px Arial`;
-
-            const width = e.measureText(token._nameplate_data.name).width;
-            const t = token.height / 2;
-            const n = e.measureText("M").width;
-
-            token._nameplate_data.position = [-width / 2 - token._nameplate_data.padding, t + token._nameplate_data.vertical_offset];
-            token._nameplate_data.size = [width + 2 * token._nameplate_data.padding, n + 2 * token._nameplate_data.padding];
-        }
-        catch (err) {
-            console.error(err);
-        }
-    };
-
-    private prepNameplateText = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
-        try {
-            let scale = this.getScale(token) - 1;
-            token._nameplate_data.position[1] += token._nameplate_data.font_size * scale;
-        }
-        catch (err) {
-            console.error(err);
-        }
-    };
-
-    public setup() {
-        window.r20es.prepNameplateBack = this.prepNameplateBack;
-        window.r20es.prepNameplateText= this.prepNameplateText;
-        R20.renderAll();
+    if(typeof(nameplate_div) === "object") {
+      this.set_scale(data.graphic, nameplate_div);
     }
+  }
 
-    public dispose() {
-        window.r20es.prepNameplateBack = undefined;
-        window.r20es.prepNameplateText= undefined;
+  getScale = (token: Roll20.Token) => {
+    const cfg = this.getHook().config;
+    let scale = token.attributes.width / cfg.widthThreshold;
 
-        R20.renderAll();
+    if (scale < 1 && !cfg.scaleIfSmaller) scale = 1;
+    if (scale > 1 && !cfg.scaleIfLarger) scale = 1;
+
+    return scale;
+  };
+
+  /*
+  private prepNameplateBack = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
+    try {
+      const cfg = this.getHook().config;
+      let scale = this.getScale(token);
+
+      const scaledFontSize = token._nameplate_data.font_size * scale;
+      e.font = `bold ${scaledFontSize}px Arial`;
+
+      const width = e.measureText(token._nameplate_data.name).width;
+      const t = token.height / 2;
+      const n = e.measureText("M").width;
+
+      token._nameplate_data.position = [-width / 2 - token._nameplate_data.padding, t + token._nameplate_data.vertical_offset];
+      token._nameplate_data.size = [width + 2 * token._nameplate_data.padding, n + 2 * token._nameplate_data.padding];
     }
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+  private prepNameplateText = (token: Roll20.CanvasObject, e: CanvasRenderingContext2D) => {
+    try {
+      let scale = this.getScale(token) - 1;
+      token._nameplate_data.position[1] += token._nameplate_data.font_size * scale;
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+  */
+
+  public setup() {
+    window.r20es.doNameplateScaling = this.doNameplateScaling;
+    this.rerender_scales();
+  }
+
+  public dispose() {
+    window.r20es.doNameplateScaling = undefined;
+    this.clear_scales();
+  }
 }
 
 export default () => {
