0c3e89e7382377da621d77eeb65fb8aa1d3de5d1
Justas Dabrila <justasdabrila@gmail.com>
2023-05-11 07:25:49 +0300

    Remove debug f&r

--- src/modules/ArrowKeysMoveCanvas/Config.ts
+++ src/modules/ArrowKeysMoveCanvas/Config.ts
@@ -7,18 +7,4 @@ export default <VTTES.Module_Config> {
   description: "Allows the camera to be moved by pressing the arrow keys as long as a token is not selected",
   category: VTTES.Module_Category.canvas,
   gmOnly: false,
-
-  mods: [
-    {
-      includes: "vtt.bundle.js",
-
-      find_replace: [
-        {
-          find: `bind:function(Q,G,Y){`,
-          replace: `bind:function(Q,G,Y) { console.log("MOUSETRAP", Q, G, Y); debugger;`,
-        },
-      ],
-    },
-  ],
-
 };
