3c6a9a0aabe98ece58b9b06398c7d921c62ac5c7
Justas Dabrila <justasdabrila@gmail.com>
2023-08-14 16:19:53 +0300

    Fix roll20's code

--- src/modules/TokenContextMenuApi/Module.tsx
+++ src/modules/TokenContextMenuApi/Module.tsx
@@ -3,77 +3,121 @@ import { R20 } from "../../utils/R20";
 import { DOM } from "../../utils/DOM";
 import { findByIdAndRemove } from "../../utils/MiscUtils";
 import { TokenContextMenu } from "../../utils/TokenContextMenu";
+import { replaceAll} from "../../utils/MiscUtils";
 
 class TokenContextMenuApiModule extends R20Module.SimpleBase {
-    private _observer: MutationObserver;
+  private _observer: MutationObserver;
 
-    public constructor() {
-        super(__dirname);
+  public constructor() {
+    super(__dirname);
+  }
+
+  private tryInsertMenuWidgets(target: HTMLElement) {
+    if(!target.className) return false;
+    if(target.className !== "actions_menu d20contextmenu")  return false;
+
+    const all = TokenContextMenu.getInternalData().widgets;
+    const selection = R20.getSelectedTokens();
+
+    for(const key in all) {
+      const data = all[key];
+
+      if(data.options && data.options.mustHaveSelection && selection.length <= 0) {
+        continue;
+      }
+
+      if(data.options && data.options.cannotHaveSelection && selection.length > 0) {
+        continue
+      }
+
+      const clicked = (e) => {
+        e.stopPropagation();
+
+        R20.hideTokenRadialMenu();
+        R20.hideTokenContextMenu();
+
+        data.callback();
+      };
+
+      const widget = <li style={{maxWidth: "99px"}} id={data.id} onClick={clicked} class='head hasSub'>{data.text}</li>;
+      target.firstElementChild.appendChild(widget);
     }
 
-    private tryInsertMenuWidgets(target: HTMLElement) {
+    return true;
+  }
 
-        if(!target.className) return false;
-        if(target.className !== "actions_menu d20contextmenu")  return false;
-
-        const all = TokenContextMenu.getInternalData().widgets;
-        const selection = R20.getSelectedTokens();
-
-        for(const key in all)
-        {
-            const data = all[key];
-
-            if(data.options && data.options.mustHaveSelection && selection.length <= 0) {
-                continue;
-            }
-
-            if(data.options && data.options.cannotHaveSelection && selection.length > 0) {
-                continue
-            }
-
-            const clicked = (e) => {
-                e.stopPropagation();
-
-                R20.hideTokenRadialMenu();
-                R20.hideTokenContextMenu();
-
-                data.callback();
-            };
-
-            const widget = <li style={{maxWidth: "99px"}} id={data.id} onClick={clicked} class='head hasSub'>{data.text}</li>;
-            target.firstElementChild.appendChild(widget);
+  private observerCallback = (muts) => {
+    for(let e of muts) {
+      for(const added of e.addedNodes) {
+        if(this.tryInsertMenuWidgets(added)) {
+            return;
         }
+      }
+    }
+  };
 
-        return true;
+  original_actions_menu_template: string | null = null;
+
+  try_get_actions_menu_template() {
+    const template = document.body.querySelector("#tmpl_actions_menu");
+    if(template == null) {
+      // TODO
+    }
+    return template as HTMLElement | null;
+  }
+
+  public setup() {
+    this._observer = new MutationObserver(this.observerCallback);
+    this._observer.observe(document.body, { childList: true, subtree: true });
+
+    var template = this.try_get_actions_menu_template();
+    if(template) {
+      console.log(template)
+      let text = template.innerHTML;
+      console.log(text);
+
+      this.original_actions_menu_template = text;
+
+      // NOTE(justasd): 'this' and 'this.get' may not exist. Roll20 skimped on these checks at
+      // around the 2023-08-10 update which added the Open Character entry I believe and so they
+      // broke right-clicking on empty canvas space. In that context, the this may not exist so this
+      // breaks the template instantiation.
+      //
+      // Ugh.
+      //
+      // Anyway they might not fix it for a while as this doesn't seem like an important thing
+      // anymore given the new UI rework, so we'll keep this in here.
+      //
+      // 2023-08-14
+      text = replaceAll(text, `this.get("represents")`, `(this && this.get && this.get("represents"))`)
+
+      console.log(text);
+
+      template.innerHTML = text;
+    }
+  }
+
+  public dispose() {
+    const all = TokenContextMenu.getInternalData().widgets;
+
+    if(this.original_actions_menu_template != null) {
+      let template = this.try_get_actions_menu_template();
+
+      if(template != null) {
+        template.innerHTML = this.original_actions_menu_template;
+        this.original_actions_menu_template = null;
+      }
     }
 
-    private observerCallback = (muts) => {
-        for(let e of muts) {
-            for(const added of e.addedNodes) {
-                if(this.tryInsertMenuWidgets(added)) {
-                    return;
-                }
-            }
-        }
-    };
-
-    public setup() {
-        this._observer = new MutationObserver(this.observerCallback);
-        this._observer.observe(document.body, { childList: true, subtree: true });
+    for(const key in all) {
+      const data = all[key];
+      findByIdAndRemove(data.id);
     }
 
-    public dispose() {
-        const all = TokenContextMenu.getInternalData().widgets;
-
-        for(const key in all) {
-            const data = all[key];
-            findByIdAndRemove(data.id);
-        }
-
-        if(this._observer) {
-            this._observer.disconnect();
-        }
+    if(this._observer) {
+      this._observer.disconnect();
     }
+  }
 }
 
 export default () => {
