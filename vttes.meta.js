a737a1c3893f316fbac15fbdc9c08215ac5202e9
Justas Dabrila <justasdabrila@gmail.com>
2024-08-22 17:37:24 +0300

    script

--- vttes.meta.js
+++ vttes.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VTT Enhancement Suite
 // @namespace    https://justas-d.github.io/
-// @version      1.28.20
+// @version      1.28.21
 // @description  aka R20ES. Provides quality-of-life and workflow speed improvements to Roll20.
 // @author       @Justas_Dabrila
 // @updateURL    https://justas-d.github.io/roll20-enhancement-suite/vttes.meta.js
@@ -13,8 +13,8 @@
 // @match        https://app.roll20.net/editor/#*
 // @match        https://app.roll20.net/editor/?*
 // @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
 // @connect      cdn.roll20.net
-// @run-at       document-start
 // @webRequest [{"selector":{"include":"*://browser.sentry-cdn.com/*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://www.datadoghq-browser-agent.com/datadog-rum.js"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://cdn.userleap.com/*"},"action":"cancel"}]
@@ -26,6 +26,6 @@
 // @webRequest [{"selector":{"include":"*://app.roll20.net/v2/js/patience.js","exclude":"*://app.roll20.net/v2/js/patience.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/editor/startjs/?timestamp*","exclude":"*://app.roll20.net/editor/startjs/?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/d20/loading.js?v=11","exclude":"*://app.roll20.net/js/d20/loading.js?n=11&v=11"},"action":"cancel"}]
-// @webRequest [{"selector":{"include":"*://cdn.roll20.net/production/vtt.bundle.js","exclude":"*://cdn.roll20.net/production/vtt.bundle.js?n*"},"action":"cancel"}]
+// @webRequest [{"selector":{"include":"*://cdn.roll20.net/vtt/legacy/production/latest/vtt.bundle.*.js","exclude":"*://cdn.roll20.net/vtt/legacy/production/latest/vtt.bundle.*.js?n*"},"action":"cancel"}]
 // @webRequest [{"selector":{"include":"*://app.roll20.net/js/tutorial_tips.js","exclude":"*://app.roll20.net/js/tutorial_tips.js?n*"},"action":"cancel"}]
 // ==/UserScript==
